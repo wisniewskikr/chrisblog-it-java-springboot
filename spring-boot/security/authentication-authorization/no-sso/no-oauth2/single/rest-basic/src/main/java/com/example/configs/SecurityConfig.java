86ed8d1e454260d1352870b30dcea32ecbefb47c
Wisniewski <wisniewk@emea.roche.com>
2022-10-23 16:05:44 +0200

    Update project rest-basic

--- spring-boot/security/authentication-authorization/no-sso/no-oauth2/single/rest-basic/src/main/java/com/example/configs/SecurityConfig.java
+++ spring-boot/security/authentication-authorization/no-sso/no-oauth2/single/rest-basic/src/main/java/com/example/configs/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -39,6 +40,10 @@ public class SecurityConfig {
 	
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable();
+        
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 		http
         	.authorizeRequests((authReq) -> authReq
