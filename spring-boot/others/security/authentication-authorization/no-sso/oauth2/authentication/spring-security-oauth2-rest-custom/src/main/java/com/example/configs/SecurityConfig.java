541f112f2dd5b708f8e0f2205ee1530de90019f7
Wisniewski <wisniewk@emea.roche.com>
2022-10-24 20:00:51 +0200

    Update project spring-security-oauth2-rest-custom

--- spring-boot/others/security/authentication-authorization/no-sso/oauth2/authentication/spring-security-oauth2-rest-custom/src/main/java/com/example/configs/SecurityConfig.java
+++ spring-boot/others/security/authentication-authorization/no-sso/oauth2/authentication/spring-security-oauth2-rest-custom/src/main/java/com/example/configs/SecurityConfig.java
@@ -1,17 +1,22 @@
 package com.example.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.filters.JwtFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -24,6 +29,13 @@ public class SecurityConfig {
     private String usernameAdmin;
 	@Value(value = "${basic.password.admin}")
     private String passwordAdmin;
+	
+	private JwtFilter jwtFilter;
+    
+    @Autowired
+	public SecurityConfig(JwtFilter jwtFilter) {
+		this.jwtFilter = jwtFilter;
+	}
 
 	@Bean
     public PasswordEncoder bcryptPasswordEncoder() {
@@ -63,11 +75,16 @@ public class SecurityConfig {
 		
 		http.csrf().disable();
 		
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
 		http
         	.authorizeRequests((authReq) -> authReq
         		.antMatchers("/user").hasAnyRole("USER", "ADMIN")
         		.antMatchers("/admin").hasRole("ADMIN")
-        	);  
+        	);
+		
+		http
+    		.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         
         return http.build();
         
