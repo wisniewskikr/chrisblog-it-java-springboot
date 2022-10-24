cd37d9491e5ccd60217f9fd82645a197d9848069
Wisniewski <wisniewk@emea.roche.com>
2022-10-24 10:56:17 +0200

    Init project spring-security-rest-custom

---
+++ spring-boot/others/security/authentication-authorization/no-sso/no-oauth2/single/spring-security-rest-custom/src/main/java/com/example/controllers/LoginController.java
@@ -0,0 +1,39 @@
+package com.example.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.jsons.AuthRequestJson;
+
+@RestController
+public class LoginController {
+	
+	private final AuthenticationManager authorizationManager;
+	
+	@Autowired
+	public LoginController(AuthenticationManager authorizationManager) {
+		this.authorizationManager = authorizationManager;
+	}
+
+	@PostMapping("/auth/login")
+    public String login(@RequestBody AuthRequestJson authRequest) {
+		
+		try {
+            
+			authorizationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+            
+        } catch (UsernameNotFoundException exception) {
+        	throw new UsernameNotFoundException("User with email not found: " + authRequest.getUsername());
+        }
+		
+		return "You are successfully logged!";
+		
+	}
+
+}
