24016fadf953de67283ae7ddb5c479c3227623df
Wisniewski <wisniewk@emea.roche.com>
2022-10-25 07:22:54 +0200

    Init project spring-security-oauth2-rest-basic

---
+++ spring-boot/others/security/authentication-authorization/no-sso/oauth2/authentication/spring-security-oauth2-rest-basic/src/main/java/com/example/configs/SecurityConfig.java
@@ -0,0 +1,93 @@
+package com.example.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.filters.JwtFilter;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Value(value = "${basic.username.user}")
+    private String usernameUser;
+	@Value(value = "${basic.password.user}")
+    private String passwordUser;
+	@Value(value = "${basic.username.admin}")
+    private String usernameAdmin;
+	@Value(value = "${basic.password.admin}")
+    private String passwordAdmin;
+	
+	private JwtFilter jwtFilter;
+    
+    @Autowired
+	public SecurityConfig(JwtFilter jwtFilter) {
+		this.jwtFilter = jwtFilter;
+	}
+
+	@Bean
+    public PasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+	@Bean
+    public AuthenticationManager authorizationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+	
+	@Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {		
+		
+        UserDetails user = User
+        	.withUsername(usernameUser)
+            .password(bcryptPasswordEncoder().encode(passwordUser))
+            .roles("USER")
+            .build();
+        
+        UserDetails admin = User
+            	.withUsername(usernameAdmin)
+                .password(bcryptPasswordEncoder().encode(passwordAdmin))
+                .roles("ADMIN")
+                .build();
+        
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(user);
+        manager.createUser(admin);
+        
+        return manager;
+        
+    }
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable();
+		
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http
+        	.authorizeRequests((authReq) -> authReq
+        		.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+        		.antMatchers("/admin").hasRole("ADMIN")
+        	);
+		
+		http
+    		.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        
+        return http.build();
+        
+    }
+
+}
---
+++ spring-boot/others/security/authentication-authorization/no-sso/oauth2/authentication/spring-security-oauth2-rest-basic/src/main/java/com/example/controllers/LoginController.java
@@ -0,0 +1,79 @@
+package com.example.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.jsons.AuthRequestJson;
+
+import javax.servlet.ServletException;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+
+@RestController
+public class LoginController {
+	
+	@Value("${token.secret.key}")
+	private String tokenSecretKey;
+	
+	private final AuthenticationManager authorizationManager;
+	
+	@Autowired
+	public LoginController(AuthenticationManager authorizationManager) {
+		this.authorizationManager = authorizationManager;
+	}
+
+	@PostMapping("/auth/login")
+    public String login(@RequestBody AuthRequestJson authRequest) throws ServletException {
+		
+		String token = null;
+		
+		try {
+            
+			UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword());
+			Authentication authenticate = authorizationManager.authenticate(usernamePasswordAuthenticationToken);
+			token = generateToken(authenticate);
+			            
+        } catch (UsernameNotFoundException exception) {
+        	throw new UsernameNotFoundException("User with following username not found: " + authRequest.getUsername());
+        } catch (ServletException e) {
+			throw e;
+		}
+		
+		return token;
+		
+	}
+	
+	private String generateToken(Authentication authenticate) throws ServletException {
+		
+		String token = null;
+		
+		GrantedAuthority[] grantedAuthorities = authenticate.getAuthorities().toArray(new GrantedAuthority[authenticate.getAuthorities().size()]);
+		
+		try {			
+			
+			Algorithm algorithm = Algorithm.HMAC256(tokenSecretKey);
+		    token = JWT.create()
+		    		.withClaim("name", authenticate.getName())
+		    		.withClaim("role", grantedAuthorities[0].getAuthority())
+		    		.sign(algorithm);
+		    
+		} catch (JWTCreationException exception){
+			throw new ServletException("Problem with creation of token");
+		}
+		
+		return token;
+		
+	}
+
+}
