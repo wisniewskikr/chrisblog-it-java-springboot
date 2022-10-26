ef4926095aaa4602c8442afd99acc1a870b78a9d
Wisniewski <wisniewk@emea.roche.com>
2022-10-26 09:01:18 +0200

    Update project spring-security-rest-basic-custom

--- spring-boot/others/security/authentication-authorization/no-sso/no-oauth2/single/spring-security-rest-basic-custom/src/main/java/com/example/controllers/LoginController.java
+++ spring-boot/others/security/authentication-authorization/no-sso/no-oauth2/single/spring-security-rest-basic-custom/src/main/java/com/example/controllers/LoginController.java
@@ -1,15 +1,18 @@
 package com.example.controllers;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.jsons.AuthRequestJson;
 
 @RestController
 public class LoginController {
@@ -21,20 +24,40 @@ public class LoginController {
 		this.authorizationManager = authorizationManager;
 	}
 
-	@PostMapping("/auth/login")
-    public String login(@RequestBody AuthRequestJson authRequest) {
+	@GetMapping("/auth/login")
+    public String login(HttpServletRequest request) throws ServletException {
+		
+		String authorizationHeader = request.getHeader("authorization");		
+		if (authorizationHeader == null) {
+			throw new ServletException("This resource is secured by Basic Authorization");
+		}
 		
 		try {
             
-			UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword());
+			UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = getUsernamePasswordAuthenticationToken(authorizationHeader);
 			authorizationManager.authenticate(usernamePasswordAuthenticationToken);			
 			SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             
         } catch (UsernameNotFoundException exception) {
-        	throw new UsernameNotFoundException("User with following username not found: " + authRequest.getUsername());
+        	throw new UsernameNotFoundException("Wrong username or password");
         }
 		
 		return "You are successfully logged in!";
+		
+	}
+	
+	private UsernamePasswordAuthenticationToken getUsernamePasswordAuthenticationToken(String authorizationHeader) throws ServletException {
+		
+		UsernamePasswordAuthenticationToken authentication = null;
+		
+		String credentialsEncoded = authorizationHeader.substring(6);		
+		byte[] credentialsDecoded = Base64.getDecoder().decode(credentialsEncoded);
+		String credentials = new String(credentialsDecoded, StandardCharsets.UTF_8);
+		String username = credentials.split(":")[0];
+		String password = credentials.split(":")[1];
+		authentication = new UsernamePasswordAuthenticationToken(username, password);
+		
+		return authentication;
 		
 	}
 
