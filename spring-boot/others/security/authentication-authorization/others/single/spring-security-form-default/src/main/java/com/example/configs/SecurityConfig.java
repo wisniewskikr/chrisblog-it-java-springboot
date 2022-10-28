0493c1d57b7129bd72e0ed9fc2e4f90ba39a7b38
Wisniewski <wisniewk@emea.roche.com>
2022-10-28 06:34:44 +0200

    Update project spring-security-form-default

--- spring-boot/others/security/authentication-authorization/others/single/spring-security-form-default/src/main/java/com/example/configs/SecurityConfig.java
+++ spring-boot/others/security/authentication-authorization/others/single/spring-security-form-default/src/main/java/com/example/configs/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -55,17 +54,13 @@ public class SecurityConfig {
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
-		http.csrf().disable();
-        
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		
 		http
         	.authorizeRequests((authReq) -> authReq
         		.antMatchers("/user").hasAnyRole("USER", "ADMIN")
         		.antMatchers("/admin").hasRole("ADMIN")
         	);  
 		
-		http.httpBasic();
+		http.formLogin();
         
         return http.build();
         
--- spring-boot/others/security/authentication-authorization/others/single/spring-security-form-default/src/main/java/com/example/controllers/HelloWorldController.java
+++ spring-boot/others/security/authentication-authorization/others/single/spring-security-form-default/src/main/java/com/example/controllers/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
 	@GetMapping("/")
 	public String helloWorld() {		
-		return "Hello World!";		
+		return "helloworld";		
 	}
 	
 	@GetMapping("/user")
