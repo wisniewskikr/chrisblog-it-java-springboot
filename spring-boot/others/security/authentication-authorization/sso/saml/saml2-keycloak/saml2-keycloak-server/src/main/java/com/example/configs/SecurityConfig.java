979e1cfba94182e0f6732a2a088dda42e874ae72
Wisniewski <wisniewk@emea.roche.com>
2022-11-01 07:51:05 +0100

    Update project 3saml2-keycloak

--- spring-boot/others/security/authentication-authorization/sso/saml/saml2-keycloak/saml2-keycloak-server/src/main/java/com/example/configs/SecurityConfig.java
+++ spring-boot/others/security/authentication-authorization/sso/saml/saml2-keycloak/saml2-keycloak-server/src/main/java/com/example/configs/SecurityConfig.java
@@ -1,43 +1,25 @@
 package com.example.configs;
 
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@KeycloakConfiguration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
-	
-	@Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder builder) {
-        builder.authenticationProvider(this.keycloakAuthenticationProvider());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-    	
-        super.configure(http);
-        
-        http.authorizeRequests()
-        	.antMatchers("*/**").authenticated();
-        
-        http.formLogin();
+protected void configure(HttpSecurity http) throws Exception {
 		
-		http.logout().logoutUrl("/logout");
+	http
+		.authorizeRequests(authorize -> authorize
+			.anyRequest().authenticated()
+		)
+		.saml2Login(Customizer.withDefaults());
 		
-		http.exceptionHandling().accessDeniedPage("/access-denied");
-        
-    }
+	}
 
 }
