b204fcb3acbe8ac4d6704fa1f4ce9ccb357d4c27
Wisniewski <wisniewk@emea.roche.com>
2023-01-24 15:56:40 +0100

    Update project springboot-helloworld-port-uuid-multiple

--- spring-boot/arichitectures/rest/springboot-helloworld-port-uuid-multiple/springboot-helloworld-port-uuid-multiple-be/src/main/java/com/example/controllers/HelloWorldBeController.java
+++ spring-boot/arichitectures/rest/springboot-helloworld-port-uuid-multiple/springboot-helloworld-port-uuid-multiple-be/src/main/java/com/example/controllers/HelloWorldBeController.java
@@ -8,12 +8,12 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.jsons.HelloWorldJson;
+import com.example.jsons.HelloWorldBeJson;
 
 @RestController
-public class HelloWorldController {
+public class HelloWorldBeController {
 	
-	Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+	Logger logger = LoggerFactory.getLogger(HelloWorldBeController.class);
 	
 	private Environment environment;
 	
@@ -21,19 +21,19 @@ public class HelloWorldController {
 	private String message;
 	
 	@Autowired
-	public HelloWorldController(Environment environment) {
+	public HelloWorldBeController(Environment environment) {
 		this.environment = environment;
 	}
 
 	@RequestMapping(value="/")
-	public HelloWorldJson helloWorld() {
+	public HelloWorldBeJson helloWorld() {
 				
 		String port = environment.getProperty("local.server.port");
 		String uuid = System.getProperty("uuid");
 		
-		logger.info("Application was called with message: {}, port: {} and uuid: {}", message, port, uuid);
+		logger.info("Application BE was called with message: {}, port BE: {} and uuid BE: {}", message, port, uuid);
 		
-		return new HelloWorldJson(message, port, uuid);
+		return new HelloWorldBeJson(message, port, uuid);
 		
 	}
 	
