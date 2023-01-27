3663fdce291ab31edf326dd26c756333b2456caa
Wisniewski <wisniewk@emea.roche.com>
2023-01-27 13:40:55 +0100

    Update project mvc-thymeleaf-form-utils-file-service

--- spring-boot/others/others/mvc-thymeleaf-form-utils-file-service/src/main/java/com/example/app/services/file/FileService.java
+++ spring-boot/others/others/mvc-thymeleaf-form-utils-file-service/src/main/java/com/example/app/services/file/FileService.java
@@ -1,21 +1,25 @@
-package com.example.app.services.file;
+package com.example.services;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class FileService {
 
-	public void writeToFile(String filePath, String content) {
+	public void writeToFile(String filePath, String content) {		
 				
 		FileWriter in = null;
 		try {
 			
-			in = new FileWriter(filePath);
+			Path path = getPath(filePath);			
+			in = new FileWriter(path.toString());
 			in.write(content);
 			
 		} catch (Exception e) {
@@ -28,13 +32,14 @@ public class FileService {
 	
 	public String readFromFile(String filePath) {
 		
+		Path path = Paths.get(filePath);
 		StringBuilder stringBuilder = new StringBuilder();
 		String line;
 		BufferedReader in = null;
 		
 		try {
 			
-			in = new BufferedReader(new FileReader(filePath));
+			in = new BufferedReader(new FileReader(path.toString()));
 			while ((line = in.readLine()) != null) {
 				stringBuilder.append(line);
 				stringBuilder.append(System.lineSeparator());				
@@ -71,6 +76,21 @@ public class FileService {
 				System.err.println(e);
 			}
 		}
+		
+	}
+	
+	private Path getPath(String filePath) throws IOException {
+		
+		Path path = Paths.get(filePath);
+		createFolder(filePath, path);
+		return path;
+		
+	}
+	
+	private void createFolder(String filePath, Path path) throws IOException {
+		
+		Path folderPath = Paths.get(filePath.replace(path.getFileName().toString(), ""));
+		Files.createDirectories(folderPath);
 		
 	}
 	
