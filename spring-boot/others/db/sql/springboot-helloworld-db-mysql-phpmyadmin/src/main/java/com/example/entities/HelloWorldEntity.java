e5e177c36c722dfec52f304b52902b5329cb8f9c
Wisniewski <wisniewk@emea.roche.com>
2023-02-11 17:44:48 +0100

    Init project springboot-helloworld-db-mysql-phpmyadmin

---
+++ spring-boot/others/db/sql/springboot-helloworld-db-mysql-phpmyadmin/src/main/java/com/example/entities/HelloWorldEntity.java
@@ -0,0 +1,35 @@
+package com.example.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="HELLO_WORLDS")
+public class HelloWorldEntity {
+		
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+ 
+    @Column(nullable = false)
+    private String text;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
