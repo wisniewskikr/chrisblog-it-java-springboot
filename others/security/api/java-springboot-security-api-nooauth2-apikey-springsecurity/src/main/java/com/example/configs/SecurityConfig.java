b629455d0576b4fcc5a015954eb3633f6d888247
wisniewskikr <wisniewskikr@gmail.com>
2023-05-20 14:15:54 +0200

    Init project

---
+++ others/security/api/java-springboot-security-api-nooauth2-apikey-springsecurity/src/main/java/com/example/configs/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.example.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import com.example.filters.ApiKeyFilter;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${api.key.name}")
+    private String apiKeyName;
+
+    @Value("${api.key.value}")
+    private String apiKeyValue;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+			.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/")
+                .authenticated()
+			)
+            .addFilter(getFilter())
+            .csrf(Customizer.withDefaults());
+        
+        return http.build();
+        
+    }
+	
+	private ApiKeyFilter getFilter() {
+    	
+    	ApiKeyFilter filter = new ApiKeyFilter(apiKeyName);
+    	
+        filter.setAuthenticationManager(new AuthenticationManager() {
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                String principal = (String) authentication.getPrincipal();
+                if (!apiKeyValue.equals(principal)) {
+                    throw new BadCredentialsException("The API key was not found or not the expected value.");
+                }
+                authentication.setAuthenticated(true);
+                return authentication;
+            }
+        });
+        
+        return filter;
+    	
+    } 
+
+}
---
+++ others/security/api/java-springboot-security-api-nooauth2-apikey-springsecurity/src/main/java/com/example/filters/ApiKeyFilter.java
@@ -0,0 +1,25 @@
+package com.example.filters;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+public class ApiKeyFilter extends AbstractPreAuthenticatedProcessingFilter {
+
+    private String principalRequestHeader;
+
+    public ApiKeyFilter(String principalRequestHeader) {
+        this.principalRequestHeader = principalRequestHeader;
+    }
+
+    @Override
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+        return request.getHeader(principalRequestHeader);
+    }
+
+    @Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+        return "N/A";
+    }
+
+}
