ad7a1737c18df570fb04e80af841049633e75a1a
wisniewskikr <wisniewskikr@gmail.com>
2023-06-01 08:48:43 +0200

    Update controllers.

--- others/security/html/html-springsec-source-header-basic-authn-inmemory-authr-config/src/main/java/com/example/controllers/PublicController.java
+++ others/security/html/html-springsec-source-header-basic-authn-inmemory-authr-config/src/main/java/com/example/controllers/PublicController.java
@@ -2,13 +2,11 @@ package com.example.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value="/")
 public class PublicController {
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value="/")
 	public String display() {
 		return "public";		
 	}
