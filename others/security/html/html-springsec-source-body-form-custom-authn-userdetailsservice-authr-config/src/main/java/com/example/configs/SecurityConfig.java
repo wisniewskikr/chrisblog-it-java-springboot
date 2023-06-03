9a01f6278c1bc631fa2dbb296f95fdf984e4c650
wisniewskikr <wisniewskikr@gmail.com>
2023-06-03 07:40:09 +0200

    Init project

---
+++ others/security/html/html-springsec-source-body-form-custom-authn-userdetailsservice-authr-config/src/main/java/com/example/configs/SecurityConfig.java
@@ -0,0 +1,81 @@
+package com.example.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Value(value = "${basic.username.user}")
+    private String usernameUser;
+	@Value(value = "${basic.password.user}")
+    private String passwordUser;
+	@Value(value = "${basic.username.admin}")
+    private String usernameAdmin;
+	@Value(value = "${basic.password.admin}")
+    private String passwordAdmin;
+
+	@Bean
+    public PasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http            
+            .authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .requestMatchers("/admin").hasAnyRole("ADMIN")
+                .anyRequest().permitAll()
+			)
+            .formLogin(login -> login
+                .loginPage("/login"))
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .addLogoutHandler(new SecurityContextLogoutHandler())
+                .logoutSuccessUrl("/")
+            )
+            .exceptionHandling(exception -> exception
+                .accessDeniedPage("/access-denied")
+            )
+            .csrf(Customizer.withDefaults());
+        
+        return http.build();
+        
+    }
+	
+	@Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {		
+		
+        UserDetails user = User
+        	.withUsername(usernameUser)
+            .password(bcryptPasswordEncoder().encode(passwordUser))
+            .roles("USER")
+            .build();
+        
+        UserDetails admin = User
+            	.withUsername(usernameAdmin)
+                .password(bcryptPasswordEncoder().encode(passwordAdmin))
+                .roles("ADMIN")
+                .build();
+        
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(user);
+        manager.createUser(admin);
+        
+        return manager;
+        
+    }
+
+}
