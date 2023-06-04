6f02a2b413acbf110892823b3a8ae15934e7416a
wisniewskikr <wisniewskikr@gmail.com>
2023-06-04 17:38:01 +0200

    Update configuration

--- others/security/html/html-springsec-sso-authn-saml-okta-authr-config/src/main/java/com/example/configs/SecurityConfig.java
+++ others/security/html/html-springsec-sso-authn-saml-okta-authr-config/src/main/java/com/example/configs/SecurityConfig.java
@@ -1,34 +1,14 @@
 package com.example.configs;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 @Configuration
 public class SecurityConfig {
-	
-	@Value(value = "${basic.username.user}")
-    private String usernameUser;
-	@Value(value = "${basic.password.user}")
-    private String passwordUser;
-	@Value(value = "${basic.username.admin}")
-    private String usernameAdmin;
-	@Value(value = "${basic.password.admin}")
-    private String passwordAdmin;
-
-	@Bean
-    public PasswordEncoder bcryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -39,7 +19,7 @@ public class SecurityConfig {
                 .requestMatchers("/admin").hasAnyRole("ADMIN")
                 .anyRequest().permitAll()
 			)
-            .formLogin(Customizer.withDefaults())
+            .saml2Login(Customizer.withDefaults())
             .logout(logout -> logout
                 .logoutUrl("/logout")
                 .addLogoutHandler(new SecurityContextLogoutHandler())
@@ -51,29 +31,6 @@ public class SecurityConfig {
             .csrf(Customizer.withDefaults());
         
         return http.build();
-        
-    }
-	
-	@Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {		
-		
-        UserDetails user = User
-        	.withUsername(usernameUser)
-            .password(bcryptPasswordEncoder().encode(passwordUser))
-            .roles("USER")
-            .build();
-        
-        UserDetails admin = User
-            	.withUsername(usernameAdmin)
-                .password(bcryptPasswordEncoder().encode(passwordAdmin))
-                .roles("ADMIN")
-                .build();
-        
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(user);
-        manager.createUser(admin);
-        
-        return manager;
         
     }
 
