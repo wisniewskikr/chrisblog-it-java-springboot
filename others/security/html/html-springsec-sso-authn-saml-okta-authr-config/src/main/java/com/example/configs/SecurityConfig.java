5ad58e01a12f5f29f6db04b29d6938f8cdc3bf9f
wisniewskikr <wisniewskikr@gmail.com>
2023-06-04 18:03:51 +0200

    Add missing repositories

--- others/security/html/html-springsec-sso-authn-saml-okta-authr-config/src/main/java/com/example/configs/SecurityConfig.java
+++ others/security/html/html-springsec-sso-authn-saml-okta-authr-config/src/main/java/com/example/configs/SecurityConfig.java
@@ -3,11 +3,15 @@ package com.example.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
+@EnableWebSecurity
 @Configuration
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
     @Bean
