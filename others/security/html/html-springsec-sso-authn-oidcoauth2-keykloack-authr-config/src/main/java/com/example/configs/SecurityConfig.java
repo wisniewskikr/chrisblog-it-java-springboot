217e36ed1e947b14b0a1ff342e0f285b9a26e332
wisniewskikr <wisniewskikr@gmail.com>
2023-06-08 07:42:43 +0200

    Migrate to Java 17

--- others/security/html/html-springsec-sso-authn-oidcoauth2-keykloack-authr-config/src/main/java/com/example/configs/SecurityConfig.java
+++ others/security/html/html-springsec-sso-authn-oidcoauth2-keykloack-authr-config/src/main/java/com/example/configs/SecurityConfig.java
@@ -1,43 +1,68 @@
 package com.example.configs;
 
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+// import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+// import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+// import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+// import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @KeycloakConfiguration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+// @EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
 	
-	@Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
+	// @Override
+    // protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+    //     return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    // }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder builder) {
-        builder.authenticationProvider(this.keycloakAuthenticationProvider());
-    }
+    // @Autowired
+    // public void configureGlobal(AuthenticationManagerBuilder builder) {
+    //     builder.authenticationProvider(this.keycloakAuthenticationProvider());
+    // }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
     	
-        super.configure(http);
+    //     super.configure(http);
         
-        http.authorizeRequests()
-        	.antMatchers("*/**").authenticated();
+    //     http.authorizeRequests()
+    //     	.antMatchers("*/**").authenticated();
         
-        http.formLogin();
+    //     http.formLogin();
 		
-		http.logout().logoutUrl("/logout");
+	// 	http.logout().logoutUrl("/logout");
 		
-		http.exceptionHandling().accessDeniedPage("/access-denied");
+	// 	http.exceptionHandling().accessDeniedPage("/access-denied");
         
+    // }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http            
+            .authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .requestMatchers("/admin").hasAnyRole("ADMIN")
+                .anyRequest().permitAll()
+			)
+            .formLogin(Customizer.withDefaults())
+            .logout(logout -> logout
+                .logoutSuccessUrl("/")
+            )
+            .exceptionHandling(exception -> exception
+                .accessDeniedPage("/access-denied")
+            )
+            .csrf(Customizer.withDefaults());
+        
+        return http.build();
+
     }
 
 }
