68208ccc96bfe729059a05246659fd1d2c84e9a3
wisniewskikr <wisniewskikr@gmail.com>
2023-06-09 08:01:41 +0200

    Remove LogoutController

--- others/security/html/html-springsec-sso-authn-oidcoauth2-facebook-authr-config/src/main/java/com/example/configs/SecurityConfig.java
+++ others/security/html/html-springsec-sso-authn-oidcoauth2-facebook-authr-config/src/main/java/com/example/configs/SecurityConfig.java
@@ -1,15 +1,27 @@
 package com.example.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    ClientRegistrationRepository clientRegistrationRepository; 
+
+    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() { 
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8080/");
+        return successHandler;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,6 +37,7 @@ public class SecurityConfig {
                 .clearAuthentication(true)
                 .logoutSuccessUrl("/")
                 .deleteCookies("JSESSIONID")
+                .logoutSuccessHandler(oidcLogoutSuccessHandler())
             )         
             .exceptionHandling(exception -> exception
                 .accessDeniedPage("/access-denied")
