f8a65c4b185a465aa8736af909f16cda315c329c
wisniewskikr <wisniewskikr@gmail.com>
2023-06-13 14:41:58 +0200

    Rename application class for BE

--- arichitectures/rcp/springboot-helloworld-rcp-graphql-multiple/springboot-helloworld-rcp-graphql-multiple-be/src/main/java/com/example/HelloWorldBeApplication.java
+++ arichitectures/rcp/springboot-helloworld-rcp-graphql-multiple/springboot-helloworld-rcp-graphql-multiple-be/src/main/java/com/example/HelloWorldBeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldBeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
+		SpringApplication.run(HelloWorldBeApplication.class, args);
 	}
 
 }
