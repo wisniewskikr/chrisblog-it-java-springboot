64ece01217b350277bb5459b4a175339405a7d53
wisniewskikr <wisniewskikr@gmail.com>
2023-06-13 12:46:22 +0200

    Implement GraphQL

--- arichitectures/rcp/springboot-helloworld-rcp-graphql/src/main/java/com/example/controllers/HelloWorldController.java
+++ arichitectures/rcp/springboot-helloworld-rcp-graphql/src/main/java/com/example/controllers/HelloWorldController.java
@@ -5,12 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
 import com.example.jsons.HelloWorldJson;
 
-@RestController
+@Controller
 public class HelloWorldController {
 	
 	Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
@@ -25,7 +25,7 @@ public class HelloWorldController {
 		this.environment = environment;
 	}
 
-	@RequestMapping(value="/")
+	@QueryMapping
 	public HelloWorldJson helloWorld() {
 				
 		String port = environment.getProperty("local.server.port");
