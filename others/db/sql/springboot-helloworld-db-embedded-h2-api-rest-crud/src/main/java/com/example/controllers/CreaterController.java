03db46f56bcaed50c58c135ae9ab5f3d63a6c9c3
wisniewskikr <wisniewskikr@gmail.com>
2023-06-15 17:02:21 +0200

    Update CreateController

--- others/db/sql/springboot-helloworld-db-embedded-h2-api-rest-crud/src/main/java/com/example/controllers/CreaterController.java
+++ others/db/sql/springboot-helloworld-db-embedded-h2-api-rest-crud/src/main/java/com/example/controllers/CreaterController.java
@@ -1,16 +1,15 @@
 package com.example.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.commands.CreateCommand;
 import com.example.dtos.UserDto;
 import com.example.services.UserService;
 
-@Controller
+@RestController
 public class CreaterController {
 	
 	private UserService userService;
@@ -19,18 +18,10 @@ public class CreaterController {
 	public CreaterController(UserService userService) {
 		this.userService = userService;
 	}
-
-	@RequestMapping(value="/create", method=RequestMethod.GET)
-	public String displayPage(@ModelAttribute("command")CreateCommand command) {
-		return "create";
-	}
 	
-	@RequestMapping(value="/create", method=RequestMethod.POST)
-	public String handleCreate(@ModelAttribute("command")CreateCommand command) {
-		
-		userService.save(new UserDto(command.getName()));
-		return "redirect:/list";
-		
+	@PostMapping("/create")
+	public UserDto handleCreate(@RequestBody CreateCommand command) {		
+		return userService.save(new UserDto(command.getName()));		
 	}
 
 }
