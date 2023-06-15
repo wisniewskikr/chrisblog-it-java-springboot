a93937644c073be196b3623cadd326fd7d127bc4
wisniewskikr <wisniewskikr@gmail.com>
2023-06-15 17:05:38 +0200

    Update DeleteController

--- others/db/sql/springboot-helloworld-db-embedded-h2-api-rest-crud/src/main/java/com/example/controllers/DeleteController.java
+++ others/db/sql/springboot-helloworld-db-embedded-h2-api-rest-crud/src/main/java/com/example/controllers/DeleteController.java
@@ -1,17 +1,12 @@
 package com.example.controllers;
 
-import jakarta.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.example.commands.DeleteCommand;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import com.example.services.UserService;
 
-@Controller
+@RestController
 public class DeleteController {
 	
 	private UserService userService;
@@ -20,24 +15,12 @@ public class DeleteController {
 	public DeleteController(UserService userService) {
 		this.userService = userService;
 	}
-
-	@RequestMapping(value="/delete")
-	public String displayPage(@ModelAttribute("command")DeleteCommand command,
-			HttpSession session) {
-		
-		Long id = (Long)session.getAttribute("selectedUserId");	
-		command.setName(userService.findById(id).getName());
-		return "delete";
-		
-	}
 	
-	@RequestMapping(value="/delete", method=RequestMethod.POST)
-	public String handleButtonEdit(@ModelAttribute("command")DeleteCommand command,
-			HttpSession session) {
+	@DeleteMapping("/delete/{id}")
+	public String handleButtonEdit(@PathVariable Long id) {
 		
-		Long id = (Long)session.getAttribute("selectedUserId");	
 		userService.deleteById(id);
-		return "redirect:/list";
+		return "Deleted";
 		
 	}
 
