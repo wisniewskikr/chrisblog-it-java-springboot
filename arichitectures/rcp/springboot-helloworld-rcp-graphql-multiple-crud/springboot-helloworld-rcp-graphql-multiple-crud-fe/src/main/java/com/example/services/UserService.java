4c37faaac65d170f1be92bb8f287e2d6678d5334
wisniewskikr <wisniewskikr@gmail.com>
2023-06-16 14:41:53 +0200

    Add graphql actions

--- arichitectures/rcp/springboot-helloworld-rcp-graphql-multiple-crud/springboot-helloworld-rcp-graphql-multiple-crud-fe/src/main/java/com/example/services/UserService.java
+++ arichitectures/rcp/springboot-helloworld-rcp-graphql-multiple-crud/springboot-helloworld-rcp-graphql-multiple-crud-fe/src/main/java/com/example/services/UserService.java
@@ -21,20 +21,35 @@ public class UserService {
     }
 
     public UserDto save(UserDto userDto) {
-        GraphQLRequest request = GraphQLRequest.builder().resource("graphql/create.graphql").variables(Map.of("name", userDto.getName())).build();
-        GraphQLResponse response = graphQLWebClient.post(request).block();
-        return response.get("create", UserDto.class);
+
+        if (userDto.getId() == null) {
+            GraphQLRequest request = GraphQLRequest.builder().resource("graphql/create.graphql").variables(Map.of("name", userDto.getName())).build();
+            GraphQLResponse response = graphQLWebClient.post(request).block();
+            return response.get("create", UserDto.class);
+        } else {
+            GraphQLRequest request = GraphQLRequest.builder().resource("graphql/edit.graphql").variables(Map.of("id", userDto.getId(), "name", userDto.getName())).build();
+            GraphQLResponse response = graphQLWebClient.post(request).block();
+            return response.get("edit", UserDto.class);
+        }
+
     }
 
     public void deleteById(Long id) {
+        GraphQLRequest request = GraphQLRequest.builder().resource("graphql/delete.graphql").variables(Map.of("id", id)).build();
+        GraphQLResponse response = graphQLWebClient.post(request).block();
+        response.get("delete", null);
     }
     
     public UserDto findById(Long id) {
-        return null;
+        GraphQLRequest request = GraphQLRequest.builder().resource("graphql/view.graphql").variables(Map.of("id", id)).build();
+        GraphQLResponse response = graphQLWebClient.post(request).block();
+        return response.get("view", UserDto.class);
     }
 
     public List<UserDto> findAll() {
-        return null;
+        GraphQLRequest request = GraphQLRequest.builder().resource("graphql/list.graphql").build();
+        GraphQLResponse response = graphQLWebClient.post(request).block();
+        return response.getList("list", UserDto.class);
     }
     
 }
