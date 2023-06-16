652c235b26057d45ffbb85b4900ea9f9988ec59d
wisniewskikr <wisniewskikr@gmail.com>
2023-06-16 19:15:36 +0200

    Add proto file

--- arichitectures/rcp/grcp/springboot-helloworld-api-grcp-single/springboot-helloworld-api-grcp-single-server/src/main/java/com/example/HelloWorldApplication.java
+++ arichitectures/rcp/grcp/springboot-helloworld-api-grcp-single/springboot-helloworld-api-grcp-single-server/src/main/java/com/example/HelloWorldApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorldFeApplication {
+public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldFeApplication.class, args);
+		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
 }
