d7d9df6015ff0e497635d1015c09b6976cc90cbe
wisniewskikr <wisniewskikr@gmail.com>
2023-06-16 14:14:32 +0200

    Remove database from FE

--- arichitectures/rcp/springboot-helloworld-rcp-graphql-multiple-crud/springboot-helloworld-rcp-graphql-multiple-crud-fe/src/main/java/com/example/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repositories;
-
-import org.springframework.stereotype.Repository;
-import com.example.entities.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long>{}
