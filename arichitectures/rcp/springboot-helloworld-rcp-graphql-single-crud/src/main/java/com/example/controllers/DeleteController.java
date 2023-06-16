a51e876046fded73b94979fa4dcfdb7e2c919860
wisniewskikr <wisniewskikr@gmail.com>
2023-06-16 08:24:19 +0200

    Update DeleteController

--- arichitectures/rcp/springboot-helloworld-rcp-graphql-single-crud/src/main/java/com/example/controllers/DeleteController.java
+++ arichitectures/rcp/springboot-helloworld-rcp-graphql-single-crud/src/main/java/com/example/controllers/DeleteController.java
@@ -1,12 +1,12 @@
 package com.example.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
 import com.example.services.UserService;
 
-@RestController
+@Controller
 public class DeleteController {
 	
 	private UserService userService;
@@ -16,8 +16,8 @@ public class DeleteController {
 		this.userService = userService;
 	}
 	
-	@DeleteMapping("/delete/{id}")
-	public String delete(@PathVariable Long id) {
+	@MutationMapping
+	public String delete(@Argument Long id) {
 		
 		userService.deleteById(id);
 		return "Deleted";
