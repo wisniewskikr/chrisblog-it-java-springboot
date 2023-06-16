87d9245d6cda4d7d5e625daa91d4225fec04269f
wisniewskikr <wisniewskikr@gmail.com>
2023-06-16 14:17:05 +0200

    Init UserService

--- arichitectures/rcp/springboot-helloworld-rcp-graphql-multiple-crud/springboot-helloworld-rcp-graphql-multiple-crud-fe/src/main/java/com/example/services/UserService.java
+++ arichitectures/rcp/springboot-helloworld-rcp-graphql-multiple-crud/springboot-helloworld-rcp-graphql-multiple-crud-fe/src/main/java/com/example/services/UserService.java
@@ -5,35 +5,31 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.example.dtos.UserDto;
-import com.example.entities.UserEntity;
-import com.example.repositories.UserRepository;
+
+import graphql.kickstart.spring.webclient.boot.GraphQLWebClient;
 
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
+    private GraphQLWebClient graphQLWebClient;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserService(GraphQLWebClient graphQLWebClient) {
+        this.graphQLWebClient = graphQLWebClient;
     }
 
     public UserDto save(UserDto userDto) {
-        UserEntity userEntity = userRepository.save(new UserEntity(userDto));
-        return new UserDto(userEntity);
+        return null
     }
 
     public void deleteById(Long id) {
-        userRepository.deleteById(id);
     }
     
     public UserDto findById(Long id) {
-        UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User doesn't exist"));
-        return new UserDto(userEntity);
+        return null;
     }
 
     public List<UserDto> findAll() {
-        List<UserEntity> users = userRepository.findAll();
-        return users.stream().map(user -> new UserDto(user)).toList();
+        return null;
     }
     
 }
