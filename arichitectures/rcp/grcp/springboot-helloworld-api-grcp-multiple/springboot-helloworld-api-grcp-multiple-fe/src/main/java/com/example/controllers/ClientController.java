b1e21d1ee58f13b3e840e52e9fe0fe289c8dabae
wisniewskikr <wisniewskikr@gmail.com>
2023-06-17 13:27:29 +0200

    Move address name and port to properties

--- arichitectures/rcp/grcp/springboot-helloworld-api-grcp-multiple/springboot-helloworld-api-grcp-multiple-fe/src/main/java/com/example/controllers/ClientController.java
+++ arichitectures/rcp/grcp/springboot-helloworld-api-grcp-multiple/springboot-helloworld-api-grcp-multiple-fe/src/main/java/com/example/controllers/ClientController.java
@@ -1,5 +1,6 @@
 package com.example.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +14,16 @@ import io.grpc.ManagedChannelBuilder;
 @RestController
 public class ClientController {
 
+    @Value("${grpc.address.name}")
+    private String addressName;
+
+    @Value("${grpc.address.port}")
+    private int addressPort;
+
     @GetMapping("/")
     public String displayStranger() {
 
-        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6565).usePlaintext().build();
-
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress(addressName, addressPort).usePlaintext().build();
         HelloWorldServiceGrpc.HelloWorldServiceBlockingStub blockingStub = HelloWorldServiceGrpc.newBlockingStub(managedChannel);
         HelloWroldResponse response = blockingStub.getHelloWorld(HelloWorldRequest.newBuilder().setName("Stranger").build());
         return response.getMessage();
