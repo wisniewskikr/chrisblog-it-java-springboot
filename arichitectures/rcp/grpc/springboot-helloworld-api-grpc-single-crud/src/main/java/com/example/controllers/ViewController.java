e1643d7dc248852e11403b1682740421c73788c1
wisniewskikr <wisniewskikr@gmail.com>
2023-06-19 12:01:12 +0200

    Implement ViewController

--- arichitectures/rcp/grpc/springboot-helloworld-api-grpc-single-crud/src/main/java/com/example/controllers/ViewController.java
+++ arichitectures/rcp/grpc/springboot-helloworld-api-grpc-single-crud/src/main/java/com/example/controllers/ViewController.java
@@ -2,9 +2,14 @@ package com.example.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.dtos.UserDto;
+import com.example.grpc.UserDtoGrpc;
+import com.example.grpc.ViewServiceGrpc;
 import com.example.services.UserService;
 
-public class ViewController {
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class ViewController extends ViewServiceGrpc.ViewServiceImplBase {
 	
 	private UserService userService;
 
@@ -13,8 +18,15 @@ public class ViewController {
 		this.userService = userService;
 	}
 
-	public UserDto view(Long id) {		
-		return userService.findById(id);		
-	}
+	@Override
+	public void view(com.example.grpc.ViewCommandGrpc commandGrpc,
+        io.grpc.stub.StreamObserver<com.example.grpc.UserDtoGrpc> responseObserver) {
+
+			UserDto userDto = userService.findById(commandGrpc.getId());
+			UserDtoGrpc userDtoGrpc = UserDtoGrpc.newBuilder().setId(userDto.getId()).setName(userDto.getName()).build();
+			responseObserver.onNext(userDtoGrpc);
+            responseObserver.onCompleted();
+      
+    }
 
 }
