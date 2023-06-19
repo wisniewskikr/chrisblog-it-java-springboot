7e8b8aea004f33b5e2d3e5495fc315771b84f3c7
wisniewskikr <wisniewskikr@gmail.com>
2023-06-19 12:17:19 +0200

    Implement DeleteController

--- arichitectures/rcp/grpc/springboot-helloworld-api-grpc-single-crud/src/main/java/com/example/controllers/DeleteController.java
+++ arichitectures/rcp/grpc/springboot-helloworld-api-grpc-single-crud/src/main/java/com/example/controllers/DeleteController.java
@@ -1,9 +1,16 @@
 package com.example.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.grpc.DeleteResponseGrpc;
+import com.example.grpc.DeleteServiceGrpc;
 import com.example.services.UserService;
 
-public class DeleteController {
+import net.devh.boot.grpc.server.service.GrpcService;
+
+
+@GrpcService
+public class DeleteController extends DeleteServiceGrpc.DeleteServiceImplBase {
 	
 	private UserService userService;
 	
@@ -11,12 +18,15 @@ public class DeleteController {
 	public DeleteController(UserService userService) {
 		this.userService = userService;
 	}
-	
-	public String delete(Long id) {
-		
-		userService.deleteById(id);
-		return "Deleted";
-		
-	}
+
+	public void delete(com.example.grpc.DeleteCommandGrpc commandGrpc,
+        io.grpc.stub.StreamObserver<com.example.grpc.DeleteResponseGrpc> responseObserver) {
+      
+			userService.deleteById(commandGrpc.getId());
+			DeleteResponseGrpc response = DeleteResponseGrpc.newBuilder().setMessage("Deleted").build();
+			responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+    }
 
 }
