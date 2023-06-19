c79014b2dd95b67024b6813566ae794498acd2de
wisniewskikr <wisniewskikr@gmail.com>
2023-06-19 08:58:28 +0200

    Implement ListController

--- arichitectures/rcp/grpc/springboot-helloworld-api-grpc-single-crud/src/main/java/com/example/controllers/ListController.java
+++ arichitectures/rcp/grpc/springboot-helloworld-api-grpc-single-crud/src/main/java/com/example/controllers/ListController.java
@@ -5,7 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.example.dtos.UserDto;
 import com.example.services.UserService;
 
-public class ListController {
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import com.example.grpc.ListServiceGrpc;
+import com.example.grpc.UserDtoGrpc;
+
+@GrpcService
+public class ListController extends ListServiceGrpc.ListServiceImplBase {
 	
 	private UserService userService;
 
@@ -14,8 +20,14 @@ public class ListController {
 		this.userService = userService;
 	}
 
-	public List<UserDto> list() {		
-		return userService.findAll();
-	}
+	@Override
+	public void list(com.example.grpc.ListCommandGrpc request,
+        io.grpc.stub.StreamObserver<com.example.grpc.UserDtoGrpc> responseObserver) {
+
+			List<UserDto> list = userService.findAll();
+			list.stream().forEach(user -> responseObserver.onNext(UserDtoGrpc.newBuilder().setId(user.getId()).setName(user.getName()).build()));
+            responseObserver.onCompleted();
+      
+    }
 
 }
