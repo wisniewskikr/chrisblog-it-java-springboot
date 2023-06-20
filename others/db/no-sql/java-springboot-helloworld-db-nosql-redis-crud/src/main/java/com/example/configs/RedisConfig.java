fec642c1a90b143a41a2d88d8da7eb8635a94076
wisniewskikr <wisniewskikr@gmail.com>
2023-06-20 19:44:31 +0200

    Update RedisConfig

--- others/db/no-sql/java-springboot-helloworld-db-nosql-redis-crud/src/main/java/com/example/configs/RedisConfig.java
+++ others/db/no-sql/java-springboot-helloworld-db-nosql-redis-crud/src/main/java/com/example/configs/RedisConfig.java
@@ -2,21 +2,16 @@ package com.example.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactory);
         return template;
     }
     
