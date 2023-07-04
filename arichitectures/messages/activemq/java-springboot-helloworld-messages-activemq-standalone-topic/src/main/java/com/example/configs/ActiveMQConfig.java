553063ec6f195126af5387881c8dec8e6e2ae4c4
wisniewskikr <wisniewskikr@gmail.com>
2023-07-04 19:43:52 +0200

    Remove unused imports

--- arichitectures/messages/activemq/java-springboot-helloworld-messages-activemq-standalone-topic/src/main/java/com/example/configs/ActiveMQConfig.java
+++ arichitectures/messages/activemq/java-springboot-helloworld-messages-activemq-standalone-topic/src/main/java/com/example/configs/ActiveMQConfig.java
@@ -1,7 +1,6 @@
 package com.example.configs;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.command.ActiveMQQueue;
 import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
 
-import javax.jms.Queue;
 import javax.jms.Topic;
 
 @EnableJms
