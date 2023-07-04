286fb7888525a90f870f57fed2075a99e45f588a
wisniewskikr <wisniewskikr@gmail.com>
2023-07-04 08:55:53 +0200

    Configure standalone

--- arichitectures/messages/activemq/java-springboot-helloworld-messages-activemq-standalone/src/main/java/com/example/configs/ActiveMQConfig.java
+++ arichitectures/messages/activemq/java-springboot-helloworld-messages-activemq-standalone/src/main/java/com/example/configs/ActiveMQConfig.java
@@ -1,11 +1,13 @@
 package com.example.configs;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.Queue;
 import javax.jms.Topic;
@@ -20,6 +22,9 @@ public class ActiveMQConfig {
     @Value("${jms.topic.name}")
     private String topicName;
 
+    @Value("${activemq.broker-url}")
+    private String brokerUrl;
+
     @Bean
     public Queue queue() {
         return new ActiveMQQueue(queueName);
@@ -30,4 +35,16 @@ public class ActiveMQConfig {
         return new ActiveMQTopic(topicName);
     }
 
+    @Bean
+    public ActiveMQConnectionFactory activeMQConnectionFactory() {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+        factory.setBrokerURL(brokerUrl);
+        return factory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate() {
+        return new JmsTemplate(activeMQConnectionFactory());
+    }
+    
 }
