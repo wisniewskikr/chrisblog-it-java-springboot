8897f02378f22e69d66ba3f4f17adcd90bab4c16
wisniewskikr <wisniewskikr@gmail.com>
2023-07-04 19:30:32 +0200

    Remove Queue code

--- arichitectures/messages/activemq/java-springboot-helloworld-messages-activemq-inmemory-topic/src/main/java/com/example/controllers/HelloWorldController.java
+++ arichitectures/messages/activemq/java-springboot-helloworld-messages-activemq-inmemory-topic/src/main/java/com/example/controllers/HelloWorldController.java
@@ -19,32 +19,27 @@ public class HelloWorldController {
 	Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 	
     private JmsTemplate jmsTemplate;
-
-    private Queue queue;
-
+	
 	private Topic topic;
 	
 	@Value("${service.helloworld.message}")
 	private String message;
 	
 	@Autowired
-	public HelloWorldController(JmsTemplate jmsTemplate, Queue queue, Topic topic) {
+	public HelloWorldController(JmsTemplate jmsTemplate, Topic topic) {
 		this.jmsTemplate = jmsTemplate;
-		this.queue = queue;
 		this.topic = topic;
 	}
 
-	// ***** TOPIC ***** //
-
-	@GetMapping("/topic/publish")
+	@GetMapping("/publish")
 	public ResponseEntity<String> topicPublish() {
 				
 		jmsTemplate.convertAndSend(topic, message);
-		return ResponseEntity.ok("Topic was pulblished successfuly.");
+		return ResponseEntity.ok("Topic was published successfuly.");
 		
 	}	
 
-	@GetMapping("/topic/subscribe")
+	@GetMapping("/subscribe")
 	public ResponseEntity<String> topicSubscribeSubscriber() {				
 		
 		jmsTemplate.setReceiveTimeout(JmsTemplate.RECEIVE_TIMEOUT_NO_WAIT);		
@@ -64,38 +59,6 @@ public class HelloWorldController {
 	@JmsListener(destination = "${jms.topic.name}")
     public void topicSubscribeListener(String message) {
         logger.info("Topic was subscribed successfuly by Listener. Message: " + message);
-    }
-
-	// ***** QUEUE ***** //
-
-	@GetMapping("/queue/produce")
-	public ResponseEntity<String> queueProduce() {
-				
-		jmsTemplate.convertAndSend(queue, message);
-		return ResponseEntity.ok("Queue was produced successfuly.");
-		
-	}	
-
-	@GetMapping("/queue/consume")
-	public ResponseEntity<String> queueConsumeConsumer() {				
-		
-		jmsTemplate.setReceiveTimeout(JmsTemplate.RECEIVE_TIMEOUT_NO_WAIT);		
-		String message = (String)jmsTemplate.receiveAndConvert(queue);
-
-   		String response = null;
-		if (message == null) {
-			response = "Queue wasn't consumed successfuly by Consumer. Message is empty";
-		} else {
-			response = "Queue was consumed successfuly by Consumer. Message: " + message;
-		}		
-
-		return ResponseEntity.ok(response);
-		
-	}
-	
-	@JmsListener(destination = "${jms.queue.name}")
-    public void queueConsumeListener(String message) {
-        logger.info("Queue was consumed successfuly by Listener. Message: " + message);
     }
 	
 }
