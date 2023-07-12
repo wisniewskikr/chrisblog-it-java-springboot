a86dfe4e06eb2f0869d3af28f60f749bd4324903
wisniewskikr <wisniewskikr@gmail.com>
2023-07-12 10:03:26 +0200

    Implement WebFlux

--- arichitectures/server-client/java-springboot-sse-display/src/main/java/com/example/controllers/ChatMessageController.java
+++ arichitectures/server-client/java-springboot-sse-display/src/main/java/com/example/controllers/ChatMessageController.java
@@ -1,96 +1,52 @@
 package com.example.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.models.ChatMessageModel;
-import com.example.models.ReadRequestModel;
-import com.example.models.SendResponseModel;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletResponse;
+import reactor.core.publisher.Flux;
+import java.time.Duration;
 
 @RestController
 public class ChatMessageController {
+
+    private List<String> messages = new ArrayList<String>();
+
+    @PostConstruct
+	public void init() {
+        messages.add("Hi");
+        messages.add("Hello World from Server-Sent Events (SSE)");
+        messages.add("This are authomatic messages sent by Server");
+        messages.add("These messages are sent as stream");
+        messages.add("Have a nice day");
+        messages.add("Bye");
+    }
 
     @GetMapping("/")
     public void index(HttpServletResponse response) throws IOException {
         response.sendRedirect("index.html");
     }
 
-    @PostMapping("/send")
-    public ResponseEntity<SendResponseModel> send(@RequestBody ChatMessageModel model) {
+    @GetMapping(value = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<String>> sse() {
 
-        long lastId = getLastId();        
-        long currentId = getCurrentId(lastId);        
-        saveMessage(currentId, model);
-
-        return ResponseEntity.ok(new SendResponseModel(currentId));
-
-    }
-
-    @PostMapping("/read")
-    public ResponseEntity<ChatMessageModel> read(@RequestBody ReadRequestModel model) {        
-
-        ChatMessageModel chatModel;
-        long lastId = getLastId();
-
-        if (lastId <= model.getId()) {
-            chatModel = new ChatMessageModel();
-            chatModel.setId(0L);
-            return ResponseEntity.ok(chatModel);
-        }        
-
-        long id = model.getId() + 1;
-        chatModel = readMessage(id);
-        return ResponseEntity.ok(chatModel);
-
-    }   
-
-    private long getLastId() {
-
-        long lastId;
-
-        String lastIdString = System.getProperty("lastId");
-        if (lastIdString == null) {
-            lastId = 0;
-        } else {
-            lastId = Long.valueOf(lastIdString);
-        }
-
-        return lastId;
-
-    }
-
-    private long getCurrentId(long lastId) {
-
-        lastId++;
-        long currentId = lastId;
-        return currentId;
-
-    }
-
-    private void saveMessage(long currentId, ChatMessageModel model) {
-
-        model.setId(currentId);
-        Gson gson = new GsonBuilder().create();
-        System.setProperty("lastId", String.valueOf(currentId));
-        System.setProperty(String.valueOf(currentId), gson.toJson(model));
-
-    }
-
-    private ChatMessageModel readMessage(long id) {
-
-        String modelString = System.getProperty(String.valueOf(id));
-        Gson gson = new GsonBuilder().create();
-        return gson.fromJson(modelString, ChatMessageModel.class);
+        AtomicInteger counter = new AtomicInteger(1);
+        return Flux.fromStream(messages.stream())
+                .map(line -> ServerSentEvent.<String> builder()
+                        .id(String.valueOf(counter.getAndIncrement()))
+                        .data(line)
+                        .event("lineEvent")
+                        .retry(Duration.ofMillis(1000))
+                        .build())
+                .delayElements(Duration.ofMillis(300));
 
     }
     
