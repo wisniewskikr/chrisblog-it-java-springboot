447287c490f965f3b2c8551a78720a1f27b8f776
wisniewskikr <wisniewskikr@gmail.com>
2023-07-18 13:25:14 +0200

    Init solution

--- arichitectures/api/mqtt/springboot-helloworld-api-mqtt-mosquitto/src/main/java/com/example/controllers/HelloWorldController.java
+++ arichitectures/api/mqtt/springboot-helloworld-api-mqtt-mosquitto/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,41 +1,39 @@
 package com.example.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.dtos.HelloWorldDto;
+import com.example.publishers.MqttPublisher;
 
 @RestController
 public class HelloWorldController {
 	
-	Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
-	
-	private Environment environment;
-	
 	@Value("${service.helloworld.message}")
 	private String message;
-	
+
+	private MqttPublisher publisher;
+
 	@Autowired
-	public HelloWorldController(Environment environment) {
-		this.environment = environment;
+	public HelloWorldController(MqttPublisher publisher) {
+		this.publisher = publisher;
 	}
 
-	@RequestMapping(value="/")
-	public ResponseEntity<HelloWorldDto> helloWorld() {
-				
-		String port = environment.getProperty("local.server.port");
-		String uuid = System.getProperty("uuid");
-		
-		logger.info("Application was called with message: {}, port: {} and uuid: {}", message, port, uuid);
-		
-		return ResponseEntity.ok(new HelloWorldDto(message, port, uuid));
-		
+	@GetMapping("/")
+	public ResponseEntity<?> publish(){
+
+		try {
+			publisher.senToMqtt(message, "myTopic");
+			return ResponseEntity.ok("Success");
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			return ResponseEntity.ok("fail");
+		}
+
 	}
+	
+
 	
 }
---
+++ arichitectures/api/mqtt/springboot-helloworld-api-mqtt-mosquitto/src/main/java/com/example/publishers/MqttPublisher.java
@@ -0,0 +1,12 @@
+package com.example.publishers;
+
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+
+@MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
+public interface MqttPublisher {
+    
+    void senToMqtt(String data, @Header(MqttHeaders.TOPIC) String topic);
+
+}
