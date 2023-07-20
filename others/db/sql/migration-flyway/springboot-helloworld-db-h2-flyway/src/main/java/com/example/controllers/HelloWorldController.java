b1d3870796484c2e80f9e85f470851175c9b9a6e
wisniewskikr <wisniewskikr@gmail.com>
2023-07-20 11:14:20 +0200

    Init project

---
+++ others/db/sql/migration-flyway/springboot-helloworld-db-h2-flyway/src/main/java/com/example/controllers/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.example.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.services.HelloWorldService;
+
+@RestController
+public class HelloWorldController {
+	
+	private HelloWorldService helloWorldService;
+
+	@Autowired
+	public HelloWorldController(HelloWorldService helloWorldService) {
+		this.helloWorldService = helloWorldService;
+	}
+
+	@RequestMapping(value="/")
+	public String helloWorld() {
+		
+		String text = helloWorldService.readText(1L);		
+		return text;		
+	}
+	
+}
