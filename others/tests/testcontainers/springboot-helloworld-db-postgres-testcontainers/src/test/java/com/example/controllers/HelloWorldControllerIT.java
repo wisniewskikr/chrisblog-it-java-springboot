3d3d326667dbf5bf6a5fbeb797d82bdf0564c703
wisniewskikr <wisniewskikr@gmail.com>
2023-07-20 13:04:29 +0200

    Implement test

---
+++ others/tests/testcontainers/springboot-helloworld-db-postgres-testcontainers/src/test/java/com/example/controllers/HelloWorldControllerIT.java
@@ -0,0 +1,30 @@
+package com.example.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloWorldControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void helloWorld() throws Exception {
+        mockMvc
+            .perform(MockMvcRequestBuilders
+                .get("/")
+            )
+            .andExpect(MockMvcResultMatchers
+                .content()
+                .string("Hello World!")
+            );
+    }
+    
+}
