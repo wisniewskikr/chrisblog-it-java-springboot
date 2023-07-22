f0e8330ef41b0934e8f05caa06d5b13772047114
wisniewskikr <wisniewskikr@gmail.com>
2023-07-22 12:46:25 +0200

    Add service to application

--- others/tests/integration-tests/java-springboot-helloworld-integrationtests-mock/src/main/java/com/example/controllers/HelloWorldController.java
+++ others/tests/integration-tests/java-springboot-helloworld-integrationtests-mock/src/main/java/com/example/controllers/HelloWorldController.java
@@ -2,21 +2,30 @@ package com.example.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dtos.HelloWorldDto;
+import com.example.services.MessageService;
 
 @RestController
 public class HelloWorldController {
 	
 	Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
+	private MessageService messageService;
+
+	@Autowired
+	public HelloWorldController(MessageService messageService) {
+		this.messageService = messageService;
+	}
+
 	@GetMapping("/")
 	public ResponseEntity<HelloWorldDto> helloWorld() {
 
-		String message = "Hello World!";
+		String message = messageService.getMessage();
 		
 		logger.info("Application was called with message: {}", message);
 		
--- others/tests/integration-tests/java-springboot-helloworld-integrationtests-mock/src/test/java/com/example/controllers/HelloWorldControllerUT.java
+++ others/tests/integration-tests/java-springboot-helloworld-integrationtests-mock/src/test/java/com/example/controllers/HelloWorldControllerUT.java
@@ -1,25 +1,25 @@
 package com.example.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.http.ResponseEntity;
+// import org.springframework.http.ResponseEntity;
 
-import com.example.dtos.HelloWorldDto;
+// import com.example.dtos.HelloWorldDto;
 
 public class HelloWorldControllerUT {
 
     @Test
     public void should_return_hello_world() throws Exception {
         
-        //given
-        HelloWorldController controller = new HelloWorldController();
+        // //given
+        // HelloWorldController controller = new HelloWorldController();
         
-        //when
-        ResponseEntity<HelloWorldDto> result = controller.helloWorld();
+        // //when
+        // ResponseEntity<HelloWorldDto> result = controller.helloWorld();
 
-        //then
-        assertEquals("Hello World!", result.getBody().getMessage());
+        // //then
+        // assertEquals("Hello World!", result.getBody().getMessage());
 
     }
     
