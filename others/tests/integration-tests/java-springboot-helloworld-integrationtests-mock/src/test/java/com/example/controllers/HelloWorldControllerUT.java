d477a6a16ac4da1b973e1ad7de4f36958cc179d9
wisniewskikr <wisniewskikr@gmail.com>
2023-07-22 12:55:44 +0200

    Implement unit test

--- others/tests/integration-tests/java-springboot-helloworld-integrationtests-mock/src/test/java/com/example/controllers/HelloWorldControllerUT.java
+++ others/tests/integration-tests/java-springboot-helloworld-integrationtests-mock/src/test/java/com/example/controllers/HelloWorldControllerUT.java
@@ -1,25 +1,29 @@
 package com.example.controllers;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-// import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity;
+import org.mockito.Mockito;
+import com.example.services.MessageService;
+import com.example.dtos.HelloWorldDto;
 
-// import com.example.dtos.HelloWorldDto;
 
 public class HelloWorldControllerUT {
 
     @Test
     public void should_return_hello_world() throws Exception {
         
-        // //given
-        // HelloWorldController controller = new HelloWorldController();
+        //given
+        MessageService messageService = Mockito.mock(MessageService.class);
+        Mockito.when(messageService.getMessage()).thenReturn("Hello World!");
+        HelloWorldController controller = new HelloWorldController(messageService);
         
-        // //when
-        // ResponseEntity<HelloWorldDto> result = controller.helloWorld();
+        //when
+        ResponseEntity<HelloWorldDto> result = controller.helloWorld();
 
-        // //then
-        // assertEquals("Hello World!", result.getBody().getMessage());
+        //then
+        assertEquals("Hello World!", result.getBody().getMessage());
 
     }
     
