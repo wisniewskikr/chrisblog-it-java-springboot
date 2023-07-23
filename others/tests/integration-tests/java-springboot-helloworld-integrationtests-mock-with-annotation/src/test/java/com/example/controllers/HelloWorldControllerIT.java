6629e3ec9e597e7500dd66ed09ab394680e28982
wisniewskikr <wisniewskikr@gmail.com>
2023-07-23 12:16:16 +0200

    Init project

---
+++ others/tests/integration-tests/java-springboot-helloworld-integrationtests-mock-with-annotation/src/test/java/com/example/controllers/HelloWorldControllerIT.java
@@ -0,0 +1,30 @@
+package com.example.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+import org.mockito.Mockito;
+import com.example.services.MessageService;
+import com.example.dtos.HelloWorldDto;
+
+
+public class HelloWorldControllerIT {
+
+    @Test
+    public void should_return_hello_world() throws Exception {
+        
+        //given
+        MessageService messageService = Mockito.mock(MessageService.class);
+        Mockito.when(messageService.getMessage()).thenReturn("Hello World!");
+        HelloWorldController controller = new HelloWorldController(messageService);
+        
+        //when
+        ResponseEntity<HelloWorldDto> result = controller.helloWorld();
+
+        //then
+        assertEquals("Hello World!", result.getBody().getMessage());
+
+    }
+    
+}
