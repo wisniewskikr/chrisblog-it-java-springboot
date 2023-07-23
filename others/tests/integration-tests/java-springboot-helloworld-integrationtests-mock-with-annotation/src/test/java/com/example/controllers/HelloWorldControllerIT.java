395acf89792cfa01bd39ebf5cbc944293be65c00
wisniewskikr <wisniewskikr@gmail.com>
2023-07-23 12:20:19 +0200

    Implement Mock annotation

--- others/tests/integration-tests/java-springboot-helloworld-integrationtests-mock-with-annotation/src/test/java/com/example/controllers/HelloWorldControllerIT.java
+++ others/tests/integration-tests/java-springboot-helloworld-integrationtests-mock-with-annotation/src/test/java/com/example/controllers/HelloWorldControllerIT.java
@@ -3,19 +3,25 @@ package com.example.controllers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.http.ResponseEntity;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import com.example.services.MessageService;
 import com.example.dtos.HelloWorldDto;
 
-
+@ExtendWith(MockitoExtension.class)
 public class HelloWorldControllerIT {
+
+    @Mock
+    private MessageService messageService;
 
     @Test
     public void should_return_hello_world() throws Exception {
         
         //given
-        MessageService messageService = Mockito.mock(MessageService.class);
         Mockito.when(messageService.getMessage()).thenReturn("Hello World!");
         HelloWorldController controller = new HelloWorldController(messageService);
         
