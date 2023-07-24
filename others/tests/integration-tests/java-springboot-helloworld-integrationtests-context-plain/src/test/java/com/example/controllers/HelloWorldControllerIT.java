a09677cdf0621829ebcddfb23e10521000ccacf3
wisniewskikr <wisniewskikr@gmail.com>
2023-07-24 08:14:48 +0200

    Implement test context

--- others/tests/integration-tests/java-springboot-helloworld-integrationtests-context-plain/src/test/java/com/example/controllers/HelloWorldControllerIT.java
+++ others/tests/integration-tests/java-springboot-helloworld-integrationtests-context-plain/src/test/java/com/example/controllers/HelloWorldControllerIT.java
@@ -3,27 +3,21 @@ package com.example.controllers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import com.example.services.MessageService;
 import com.example.dtos.HelloWorldDto;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class HelloWorldControllerIT {
 
-    @Mock
-    private MessageService messageService;
+    @Autowired
+    private HelloWorldController controller;
 
     @Test
     public void should_return_hello_world() throws Exception {
         
         //given
-        Mockito.when(messageService.getMessage()).thenReturn("Hello World!");
-        HelloWorldController controller = new HelloWorldController(messageService);
         
         //when
         ResponseEntity<HelloWorldDto> result = controller.helloWorld();
