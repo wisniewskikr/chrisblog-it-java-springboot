6d2a6077c80e2c77f28e22371d249e982bf99e4b
wisniewskikr <wisniewskikr@gmail.com>
2023-11-13 08:52:02 +0100

    Add controller

---
+++ arichitectures/ui/text/java-springboot-gui-text-helloworld/src/main/java/com/example/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+    
+    @GetMapping
+    public String display() {
+        return "Hello World!";
+    }
+
+}
