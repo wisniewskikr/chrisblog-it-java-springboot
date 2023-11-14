3d761b8011d5839d091d6530daa6119370cc8bbd
wisniewskikr <wisniewskikr@gmail.com>
2023-11-14 07:33:48 +0100

    Update project

--- arichitectures/ui/text/java-springboot-gui-text-helloworld/src/main/java/com/example/controllers/HelloWorldController.java
+++ arichitectures/ui/text/java-springboot-gui-text-helloworld/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,14 +1,15 @@
 package com.example.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloWorldController {
-	
 
-	@GetMapping("/")
-	public String helloWorld() {				
+	@RequestMapping
+	@ResponseBody
+	public String display() {
 		return "Hello World!";		
 	}
 	
