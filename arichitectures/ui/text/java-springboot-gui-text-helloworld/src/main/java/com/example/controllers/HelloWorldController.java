dab4a32e258d17b974ce62a04d349da508cdec71
wisniewskikr <wisniewskikr@gmail.com>
2023-11-14 07:35:26 +0100

    Update project

--- arichitectures/ui/text/java-springboot-gui-text-helloworld/src/main/java/com/example/controllers/HelloWorldController.java
+++ arichitectures/ui/text/java-springboot-gui-text-helloworld/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorldController {
 
-	@RequestMapping
+	@RequestMapping(produces = MediaType.TEXT_PLAIN_VALUE)
 	@ResponseBody
 	public String display() {
 		return "Hello World!";		
