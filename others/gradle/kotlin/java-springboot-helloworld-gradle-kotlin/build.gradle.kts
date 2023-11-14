c43fe2b36e51ccfa8e6e023f5188ed54cba4b8d8
wisniewskikr <wisniewskikr@gmail.com>
2023-11-14 08:19:41 +0100

    Init project

---
+++ others/gradle/kotlin/java-springboot-helloworld-gradle-kotlin/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.5"
+	id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+}
+
+tasks.bootBuildImage {
+	builder.set("paketobuildpacks/builder-jammy-base:latest")
+}
