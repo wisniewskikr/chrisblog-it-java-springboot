dcca6828ac569cfb53ffd8b28a75bb24c9c4c45f
wisniewskikr <wisniewskikr@gmail.com>
2024-01-11 08:48:37 +0100

    Init springboot-communication-restclient-http-interface

---
+++ others/communication/springboot-communication-restclient-http-interface/src/main/java/com/example/Application.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import com.example.client.JsonPlaceholderService;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	JsonPlaceholderService jsonPlaceholderService() {
+		RestClient client = RestClient.create("https://jsonplaceholder.typicode.com");
+	    HttpServiceProxyFactory factory = HttpServiceProxyFactory
+				.builderFor(RestClientAdapter.create(client))
+				.build();
+	    return factory.createClient(JsonPlaceholderService.class);
+	}
+
+}
