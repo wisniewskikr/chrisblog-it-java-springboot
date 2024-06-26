e2315ef7b3b748c7124f6c1ebb1e79ccc720d1bd
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-26 08:13:46 +0200

    Update logout

--- others/security/html/java-springboot3-ui-html-security-form-default-inmemory/src/main/java/com/example/configs/SecurityConfig.java
+++ others/security/html/java-springboot3-ui-html-security-form-default-inmemory/src/main/java/com/example/configs/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 @Configuration
 public class SecurityConfig {
@@ -40,10 +39,13 @@ public class SecurityConfig {
                 .anyRequest().permitAll()
 			)
             .formLogin(Customizer.withDefaults())
-            .logout(logout -> logout
-                .logoutUrl("/logout")
-                .addLogoutHandler(new SecurityContextLogoutHandler())
-                .logoutSuccessUrl("/")
+            .logout(logout -> 
+                logout
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
+                    .permitAll()
             )
             .exceptionHandling(exception -> exception
                 .accessDeniedPage("/access-denied")
