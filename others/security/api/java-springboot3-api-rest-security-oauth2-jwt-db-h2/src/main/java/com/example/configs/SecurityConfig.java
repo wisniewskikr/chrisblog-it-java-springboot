fddc88240fb2d9f374698154c5d137d71a0ec0c9
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-26 12:01:59 +0200

    Improvement of SecurityConfig

--- others/security/api/java-springboot3-api-rest-security-oauth2-jwt-db-h2/src/main/java/com/example/configs/SecurityConfig.java
+++ others/security/api/java-springboot3-api-rest-security-oauth2-jwt-db-h2/src/main/java/com/example/configs/SecurityConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -51,29 +51,17 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http            
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .headers(httpSecurityHeadersConfigurer -> {
+                httpSecurityHeadersConfigurer.frameOptions(FrameOptionsConfig::disable);
+            })
             .authorizeHttpRequests((requests) -> requests
 				.requestMatchers("/user").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                 .requestMatchers("/admin").hasAnyAuthority("ROLE_ADMIN")
                 .anyRequest().permitAll()
-			)
-            .formLogin(Customizer.withDefaults())
-            .logout(logout -> 
-                logout
-                    .logoutUrl("/logout")
-                    .logoutSuccessUrl("/")
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID")
-                    .permitAll()
-            )
-            .exceptionHandling(exception -> exception
-                .accessDeniedPage("/access-denied")
-            )
-            .csrf(csrf -> csrf
-                .ignoringRequestMatchers("/h2-console/**"))
-            .headers(httpSecurityHeadersConfigurer -> {
-                httpSecurityHeadersConfigurer.frameOptions(FrameOptionsConfig::disable);
-            })
-            .authenticationProvider(authenticationProvider());
+			)           
+            .authenticationProvider(authenticationProvider());            
         
         return http.build();
         
