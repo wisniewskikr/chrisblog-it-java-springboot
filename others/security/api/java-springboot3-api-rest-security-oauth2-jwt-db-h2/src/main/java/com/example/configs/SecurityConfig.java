79709b79bbc9d8cfed3a68c1717040b9ca26a636
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-26 12:19:02 +0200

    Implement AuthController

--- others/security/api/java-springboot3-api-rest-security-oauth2-jwt-db-h2/src/main/java/com/example/configs/SecurityConfig.java
+++ others/security/api/java-springboot3-api-rest-security-oauth2-jwt-db-h2/src/main/java/com/example/configs/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.example.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
@@ -46,6 +48,11 @@ public class SecurityConfig {
 		return authenticationProvider;
 
 	}
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
