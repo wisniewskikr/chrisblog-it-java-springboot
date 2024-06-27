507dc86de0729b087f73c7634310f904b2b4490c
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-27 08:41:27 +0200

    Remove CustomUserDetailsService

--- others/security/html/java-springboot3-ui-html-security-form-default-db-h2/src/main/java/com/example/configs/SecurityConfig.java
+++ others/security/html/java-springboot3-ui-html-security-form-default-db-h2/src/main/java/com/example/configs/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.configs;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,15 +10,18 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.entities.UserEntity;
 import com.example.repositories.UserRepository;
-import com.example.services.CustomUserDetailsService;
 
 @Configuration
 public class SecurityConfig {
@@ -29,7 +35,28 @@ public class SecurityConfig {
 
     @Bean
 	public UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService(userRepository);
+        return new UserDetailsService() {
+            
+            @Override
+            public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+                
+                UserEntity user = userRepository.findByUserName(userName);
+
+                return User
+                    .builder()
+                    .username(user.getUserName())
+                    .password(user.getPassword())
+                    .authorities( Arrays.stream(
+                                user.getRoles()
+                                .split(","))
+                                .map(SimpleGrantedAuthority::new)
+                                .collect(Collectors.toList()))
+                    .build();        
+
+            }
+
+        };
+
     }
     
     @Bean
