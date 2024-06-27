666f2a2472ef9e974e8926be4c73eae2bb50388e
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-06-27 08:51:47 +0200

    Init project

---
+++ others/security/html/java-springboot3-ui-html-security-form-custom-inmemory/src/main/java/com/example/configs/SecurityConfig.java
@@ -0,0 +1,92 @@
+package com.example.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Value(value = "${username.user}")
+    private String usernameUser;
+	@Value(value = "${password.user}")
+    private String passwordUser;
+	@Value(value = "${username.admin}")
+    private String usernameAdmin;
+	@Value(value = "${password.admin}")
+    private String passwordAdmin;
+
+	@Bean
+    public PasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+     @Bean
+	public UserDetailsService userDetailsService() {
+
+        UserDetails user = User.builder()
+		    .username(usernameUser)
+		    .password(bcryptPasswordEncoder().encode(passwordUser))
+		    .roles("USER")
+		    .build();
+	    
+        UserDetails admin = User.builder()
+		    .username(usernameAdmin)
+		    .password(bcryptPasswordEncoder().encode(passwordAdmin))
+		    .roles("USER", "ADMIN")
+		    .build();
+	    
+        return new InMemoryUserDetailsManager(user, admin);
+
+    }
+    
+    @Bean
+	public AuthenticationProvider authenticationProvider() {
+
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(bcryptPasswordEncoder());
+		return authenticationProvider;
+
+	}
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http            
+            .authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .requestMatchers("/admin").hasAnyRole("ADMIN")
+                .anyRequest().permitAll()
+			)
+            .formLogin(Customizer.withDefaults())
+            .logout(logout -> 
+                logout
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
+                    .permitAll()
+            )
+            .exceptionHandling(exception -> exception
+                .accessDeniedPage("/access-denied")
+            )
+            .csrf(Customizer.withDefaults())
+            .authenticationProvider(authenticationProvider());
+        
+        return http.build();
+        
+    }
+
+}
