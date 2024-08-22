ffbc561299b999a85fece2f2ecde37e0dc370fc6
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-22 08:49:49 +0200

    Init project

---
+++ others/db/sql/springboot2-helloworld-db-mysql-datasql/src/main/java/com/example/app/entities/GreetingEntity.java
@@ -0,0 +1,35 @@
+package com.example.app.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="GREETING")
+public class GreetingEntity {
+		
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+ 
+    @Column(nullable = false)
+    private String text;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
