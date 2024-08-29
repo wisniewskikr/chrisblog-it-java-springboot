45aab5cbb54fbbe5fa034cc7d53f6aa025628bbf
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-29 08:33:09 +0200

    Init project

---
+++ others/services/fe-angular-be-springboot-db-sql-mysql/fe-angular-be-springboot-db-sql-mysql_BE/src/main/java/com/example/entities/HelloWorldEntity.java
@@ -0,0 +1,28 @@
+package com.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "HELLOWORLD")
+public class HelloWorldEntity {
+
+    @Id
+    private Long id;
+    private String text;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }    
+
+}
