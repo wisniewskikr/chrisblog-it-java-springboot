5d3a45bcbdbc7bcffc80410a1a248294dabfef2d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-29 18:01:01 +0200

    Create HelloWorldComponent

--- others/services/fe-angular-be-springboot-db-sql-mysql/fe-angular-be-springboot-db-sql-mysql_FE/src/app/app.component.ts
+++ others/services/fe-angular-be-springboot-db-sql-mysql/fe-angular-be-springboot-db-sql-mysql_FE/src/app/app.component.ts
@@ -1,36 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ApiService } from './services/api.service';
-import { HelloWorldDto } from './dtos/HelloWorldDto';
-import { UuidService } from './services/uuid.service';
+import { HelloWorldComponent } from "./components/hello-world/hello-world.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloWorldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-
-  message: string = "";
-  idBe: string = "";
-  portBe: string = "";
-  idFe: string = "";
-  portFe: string = "";
-
-  constructor(private apiService: ApiService, private uuidSevice: UuidService) {}
-
-  ngOnInit(): void {
-    this.idFe = this.uuidSevice.getUUID();
-    this.portFe = window.location.port;
-
-    let response = this.apiService.getMessage();
-    response.subscribe((helloWorldDto: HelloWorldDto)=>{      
-      this.message = helloWorldDto.text;
-      this.idBe = helloWorldDto.idBe;
-      this.portBe = helloWorldDto.portBe;
-    });
-  }
-  
-}
+export class AppComponent {}
---
+++ others/services/fe-angular-be-springboot-db-sql-mysql/fe-angular-be-springboot-db-sql-mysql_FE/src/app/components/hello-world/hello-world.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { UuidService } from '../../services/uuid.service';
+import { HelloWorldDto } from '../../dtos/HelloWorldDto';
+
+
+@Component({
+  selector: 'hello-world',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.css'
+})
+export class HelloWorldComponent implements OnInit{
+
+  message: string = "";
+  idBe: string = "";
+  portBe: string = "";
+  idFe: string = "";
+  portFe: string = "";
+
+  constructor(private apiService: ApiService, private uuidSevice: UuidService) {}
+
+  ngOnInit(): void {
+    this.idFe = this.uuidSevice.getUUID();
+    this.portFe = window.location.port;
+
+    let response = this.apiService.getMessage();
+    response.subscribe((helloWorldDto: HelloWorldDto)=>{      
+      this.message = helloWorldDto.text;
+      this.idBe = helloWorldDto.idBe;
+      this.portBe = helloWorldDto.portBe;
+    });
+  }
+  
+}
