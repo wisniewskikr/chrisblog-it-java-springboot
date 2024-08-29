93e4c72fadd3e2b0d4901ce6c3b8c4f0100861a6
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-29 09:47:52 +0200

    Init project

--- others/services/fe-angular-be-springboot-db-sql-mysql/fe-angular-be-springboot-db-sql-mysql_FE/src/app/app.component.spec.ts
+++ others/services/fe-angular-be-springboot-db-sql-mysql/fe-angular-be-springboot-db-sql-mysql_FE/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'fe-angular-be-springboot-db-sql-mysql' title`, () => {
+  it(`should have the 'fe-angular-be-springboot-db-sql-mysql_FE' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fe-angular-be-springboot-db-sql-mysql');
+    expect(app.title).toEqual('fe-angular-be-springboot-db-sql-mysql_FE');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fe-angular-be-springboot-db-sql-mysql');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fe-angular-be-springboot-db-sql-mysql_FE');
   });
 });
