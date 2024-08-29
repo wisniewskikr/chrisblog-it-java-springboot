838d2148f842fbf8e455ea101193731232bae70d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-29 12:41:07 +0200

    Add crross origin annotation

--- others/services/fe-angular-be-springboot-db-sql-mysql/fe-angular-be-springboot-db-sql-mysql_BE/src/main/java/com/example/controllers/HelloWorldController.java
+++ others/services/fe-angular-be-springboot-db-sql-mysql/fe-angular-be-springboot-db-sql-mysql_BE/src/main/java/com/example/controllers/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class HelloWorldController {
 		this.helloWorldService = helloWorldService;
 	}
 
+	@CrossOrigin("http://localhost:4200/")
 	@GetMapping("/message/{id}")
 	public ResponseEntity<HelloWorldDto> helloWorld(@PathVariable Long id) {
 
