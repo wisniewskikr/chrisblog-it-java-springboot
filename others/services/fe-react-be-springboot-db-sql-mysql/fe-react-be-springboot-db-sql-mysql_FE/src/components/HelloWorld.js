1042302a4d256a2926ad042da6c9ebf756c1de3b
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-09-12 19:19:44 +0200

    Implement HelloWorld component

---
+++ others/services/fe-react-be-springboot-db-sql-mysql/fe-react-be-springboot-db-sql-mysql_FE/src/components/HelloWorld.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import getFixedUUID from "../services/uuidService";
+
+function HelloWorld() {
+
+  const [message, setMessage] = useState("");
+  const [idBe, setIdBe] = useState("");
+  const [portBe, setPortBe] = useState("");
+  const [idFe, setIdFe] = useState("");
+  const portFe = window.location.port;
+
+  useEffect(() =>{
+
+    const uuid = getFixedUUID();
+    setIdFe(uuid);
+    
+    const fetchData = async () => {
+
+      const response = await fetch("http://localhost:8081/message/1");
+      const result = await response.json();
+      setMessage(result.text);
+      setIdBe(result.idBe);
+      setPortBe(result.portBe);
+
+    };
+
+    fetchData();
+
+  }, []);
+
+  return (
+    <ul>
+      <li>
+          <span><b>Database Message: </b></span>
+          <span> {message} </span>
+      </li>
+      <li>
+          <span><b>Back-End Id: </b></span>
+          <span> {idBe} </span>
+      </li>
+      <li>
+          <span><b>Back-End Port: </b></span>
+          <span> {portBe} </span>
+      </li>
+      <li>
+          <span><b>Front-End Id: </b></span>
+          <span> {idFe} </span>
+      </li>
+      <li>
+          <span><b>Front-End Port: </b></span>
+          <span> {portFe} </span>
+      </li>
+    </ul>
+  );
+
+}
+
+export default HelloWorld;
