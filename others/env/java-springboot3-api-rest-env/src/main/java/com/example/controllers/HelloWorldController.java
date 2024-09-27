86e4a3d4515ae4066ad7a4d4fa05de467b18df2d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-09-27 11:47:09 +0200

    Add message env property

--- others/env/java-springboot3-api-rest-env/src/main/java/com/example/controllers/HelloWorldController.java
+++ others/env/java-springboot3-api-rest-env/src/main/java/com/example/controllers/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.example.controllers;
 import java.util.Collections;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+	@Value("${message}")
+	private String message;
+
 	@GetMapping
 	public ResponseEntity<Map<String, String>> helloWorld() {
 
-		Map<String, String> response = Collections.singletonMap("message", "Hello World!");		
+		Map<String, String> response = Collections.singletonMap("message", message);		
 		return ResponseEntity.ok(response);		
 		
 	}
