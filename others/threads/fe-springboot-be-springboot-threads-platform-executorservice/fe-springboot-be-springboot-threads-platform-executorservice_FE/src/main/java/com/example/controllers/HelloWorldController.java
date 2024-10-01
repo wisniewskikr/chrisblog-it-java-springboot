696022c9fb5e05ccdf696ae7dd433382da3700c1
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-01 12:07:59 +0200

    Implement threads in FE controller

--- others/threads/fe-springboot-be-springboot-threads-platform-executorservice/fe-springboot-be-springboot-threads-platform-executorservice_FE/src/main/java/com/example/controllers/HelloWorldController.java
+++ others/threads/fe-springboot-be-springboot-threads-platform-executorservice/fe-springboot-be-springboot-threads-platform-executorservice_FE/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,5 +1,11 @@
 package com.example.controllers;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClient;
 
@@ -13,11 +19,33 @@ public class HelloWorldController {
     }
 
     @GetMapping("/")
-    public String get() {
-        return restClient.get()
-                .uri("/")
-                .retrieve()
-                .body(String.class);
+    public String get() throws InterruptedException, ExecutionException {
+
+        String result = null;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(200);
+
+        Future<String> future = executorService.submit(
+            new Callable<String>() {
+
+                @Override
+                public String call() throws Exception {
+                    return restClient.get()
+                        .uri("/")
+                        .retrieve()
+                        .body(String.class);
+                }
+                
+            }
+
+        );
+
+        result = future.get();
+
+        executorService.shutdown();
+
+        return result;
+        
     }
 
 }
