f67ed5b3d9b651f56e88b9894c27522f3ae94916
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-01 08:12:20 +0200

    Update BE service

--- others/threads/fe-springboot-be-springboot-threads-no/fe-springboot-be-springboot-threads-no_BE/src/main/java/com/example/controllers/HelloWorldController.java
+++ others/threads/fe-springboot-be-springboot-threads-no/fe-springboot-be-springboot-threads-no_BE/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,30 +1,15 @@
 package com.example.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.dtos.HelloWorldDto;
-import com.example.services.HelloWorldService;
 
 @RestController
 public class HelloWorldController {
 
-	private HelloWorldService helloWorldService;
-
-	@Autowired
-	public HelloWorldController(HelloWorldService helloWorldService) {
-		this.helloWorldService = helloWorldService;
-	}
-
-	@GetMapping("/message/{id}")
-	public ResponseEntity<HelloWorldDto> helloWorld(@PathVariable Long id) {
-
-		HelloWorldDto helloWorldDto = helloWorldService.findById(id);
-		return ResponseEntity.ok(helloWorldDto);		
-		
+	@GetMapping("/")
+	public String helloWorld() throws InterruptedException {
+		Thread.sleep(3000);
+		return "Hello World!";			
 	}
 	
 }
--- others/threads/fe-springboot-be-springboot-threads-no/fe-springboot-be-springboot-threads-no_BE/src/main/java/com/example/dtos/HelloWorldDto.java
+++
@@ -1,3 +0,0 @@
-package com.example.dtos;
-
-public record HelloWorldDto(Long id, String text, String portBe) {}
