052c5a89bf8effa5d21f50ff82fa2938305460f4
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-08 08:40:46 +0200

    Init project

---
+++ others/init/java-springboot3-api-rest-helloworld-init/src/main/java/com/example/init/JavaSpringboot3ApiRestHelloworldInitApplication.java
@@ -0,0 +1,13 @@
+package com.example.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringboot3ApiRestHelloworldInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringboot3ApiRestHelloworldInitApplication.class, args);
+	}
+
+}
---
+++ others/init/java-springboot3-api-rest-helloworld-init/src/test/java/com/example/init/JavaSpringboot3ApiRestHelloworldInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringboot3ApiRestHelloworldInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
