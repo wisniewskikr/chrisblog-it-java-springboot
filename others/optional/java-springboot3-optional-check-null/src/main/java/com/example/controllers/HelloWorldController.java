d09a09d2515c3f822e5e181fb69e44bd557eede6
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-12 08:34:13 +0200

    Implement exception

--- others/optional/java-springboot3-optional-check-null/src/main/java/com/example/controllers/HelloWorldController.java
+++ others/optional/java-springboot3-optional-check-null/src/main/java/com/example/controllers/HelloWorldController.java
@@ -2,18 +2,25 @@ package com.example.controllers;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
 	@GetMapping
-	public ResponseEntity<Map<String, String>> helloWorld() {
+	public ResponseEntity<Map<String, String>> helloWorld(@RequestParam(name = "name", required = false) String name) {
 
-		Map<String, String> response = Collections.singletonMap("message", "Hello World!");		
+		name = Optional
+			.ofNullable(name)
+			.orElseThrow(() -> 
+            new IllegalArgumentException("Name cannot be null"));
+
+		Map<String, String> response = Collections.singletonMap("message", "Hello World: " + name + "!");		
 		return ResponseEntity.ok(response);		
 		
 	}
