178858c08d6df3719637a98889bf3b2511007ed1
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-13 13:01:33 +0200

    Implement consumer

--- others/stream/java-springboot3-stream-consumer-foreach/src/main/java/com/example/controllers/HelloWorldController.java
+++ others/stream/java-springboot3-stream-consumer-foreach/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,10 +1,10 @@
 package com.example.controllers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +16,12 @@ public class HelloWorldController {
 	@GetMapping
 	public ResponseEntity<Map<String, String>> helloWorld() {
 
-		List<String> messages = Arrays.asList("Some text");
-                
-        List<String> result = messages.stream()
-                                           .map(n -> "Hello World!")
-                                           .collect(Collectors.toList());
+		List<String> result = new ArrayList<>();
+
+		List<String> messages = Arrays.asList("Hello World!");
+		                
+        messages.stream()
+                    .forEach(result::add);
 
 		Map<String, String> response = Collections.singletonMap("message", result.get(0));		
 		return ResponseEntity.ok(response);		
