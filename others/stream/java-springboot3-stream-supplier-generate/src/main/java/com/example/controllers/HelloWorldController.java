be5667e7419f17394b3bb972a500acbbea8e6bc9
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-13 14:01:14 +0200

    Implement generate method

--- others/stream/java-springboot3-stream-supplier-generate/src/main/java/com/example/controllers/HelloWorldController.java
+++ others/stream/java-springboot3-stream-supplier-generate/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,10 +1,10 @@
 package com.example.controllers;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +16,10 @@ public class HelloWorldController {
 	@GetMapping
 	public ResponseEntity<Map<String, String>> helloWorld() {
 
-		List<String> messages = Arrays.asList("Some text");
-                
-        List<String> result = messages.stream()
-                                           .map(n -> "Hello World!")
-                                           .collect(Collectors.toList());
+        List<String> result = Stream
+                                .generate(() -> "Hello World!")
+								.limit(1)
+                                .collect(Collectors.toList());
 
 		Map<String, String> response = Collections.singletonMap("message", result.get(0));		
 		return ResponseEntity.ok(response);		
