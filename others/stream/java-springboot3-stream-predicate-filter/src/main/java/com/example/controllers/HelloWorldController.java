6f34f2bc4133403a63b7f654ddbb8efbcf9f27c7
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-13 08:54:24 +0200

    Implement stream filter

--- others/stream/java-springboot3-stream-predicate-filter/src/main/java/com/example/controllers/HelloWorldController.java
+++ others/stream/java-springboot3-stream-predicate-filter/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,27 +1,28 @@
 package com.example.controllers;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
 	@GetMapping
-	public ResponseEntity<Map<String, String>> helloWorld(@RequestParam(name = "name", required = false) String name) {
+	public ResponseEntity<Map<String, String>> helloWorld() {
 
-		if (!Optional.ofNullable(name).isPresent()) {
-			Map<String, String> response = Collections.singletonMap("message", "Name cannot be null!");		
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-		}
+		List<String> messages = Arrays.asList("Some text", "Hello World!", "Some text");
+                
+        List<String> result = messages.stream()
+                                           .filter("Hello World!"::equals)
+                                           .collect(Collectors.toList());
 
-		Map<String, String> response = Collections.singletonMap("message", "Hello World " + name + "!");		
+		Map<String, String> response = Collections.singletonMap("message", result.get(0));		
 		return ResponseEntity.ok(response);		
 		
 	}
