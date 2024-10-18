a3359ba753e35ea0e0948845635c55ebb6b07354
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-18 17:26:17 +0200

    Init project

---
+++ others/threads-inner/springboot-threads-inner-completablefuture-thenaccept/springboot-threads-inner-completablefuture-thenaccept_BE/src/main/java/com/example/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/{name}")
+	public String helloWorld(@PathVariable("name") String name) throws InterruptedException {
+		Thread.sleep(3000);
+		return "Tmp " + name + "!";			
+	}
+	
+}
---
+++ others/threads-inner/springboot-threads-inner-completablefuture-thenaccept/springboot-threads-inner-completablefuture-thenaccept_FE/src/main/java/com/example/controllers/HelloWorldController.java
@@ -0,0 +1,55 @@
+package com.example.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.services.ApiService;
+
+@RestController
+public class HelloWorldController {
+
+    private ApiService apiService;
+
+    public HelloWorldController(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    @GetMapping("/")
+    public String get() throws InterruptedException, ExecutionException {
+
+        List<String> results = new ArrayList<>();        
+
+        long startTime = System.currentTimeMillis();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        
+        CompletableFuture<String> future1 = apiService.callApi(executorService).thenApply(result -> result.replace("Tmp", "Hello World"));
+        CompletableFuture<String> future2 = apiService.callApi(executorService).thenApply(result -> result.replace("Tmp", "Hello World"));
+        CompletableFuture<String> future3 = apiService.callApi(executorService).thenApply(result -> result.replace("Tmp", "Hello World"));
+
+        CompletableFuture.allOf(future1, future2, future3).join();
+
+        results.add(future1.get());
+        results.add(future2.get());
+        results.add(future3.get());
+        
+        executorService.shutdown();
+
+        long endTime = System.currentTimeMillis();
+
+        JSONObject json = new JSONObject();
+        json.put("result", String.join(", ", results));
+        json.put("duration in ms", (endTime - startTime));
+
+        return json.toString();
+        
+    }
+
+}
