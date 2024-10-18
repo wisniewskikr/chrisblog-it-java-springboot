8764af46041f8a45000369b408092415c05c2cc4
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-18 16:34:13 +0200

    Init project

---
+++ others/threads-inner/springboot-threads-inner-completablefuture-supplyasync/springboot-threads-inner-completablefuture-supplyasync_FE/src/main/java/com/example/services/ApiService.java
@@ -0,0 +1,33 @@
+package com.example.services;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class ApiService {
+
+    private RestClient restClient;    
+
+    public ApiService(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.baseUrl("http://localhost:8081").build();
+    }
+    
+    public CompletableFuture<Void> callApi(List<String> results, ExecutorService executor) {
+        
+        return CompletableFuture.runAsync(() -> {
+
+            String result = restClient.get()
+                .uri("/" + Thread.currentThread().getName())
+                .retrieve()
+                .body(String.class);
+            results.add(result);
+             
+        }, executor);
+
+    }
+
+}
