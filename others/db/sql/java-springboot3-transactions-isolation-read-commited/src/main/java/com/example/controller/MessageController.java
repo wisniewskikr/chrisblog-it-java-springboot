960b0e44742d514101d17733dfa37f9319f2b51c
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-21 16:14:56 +0200

    Add logger

--- others/db/sql/java-springboot3-transactions-isolation-read-commited/src/main/java/com/example/controller/MessageController.java
+++ others/db/sql/java-springboot3-transactions-isolation-read-commited/src/main/java/com/example/controller/MessageController.java
@@ -2,28 +2,18 @@ package com.example.controller;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.model.dto.MessageDto;
 import com.example.model.dto.ResponseDto;
 import com.example.service.MessageService;
 
-import jakarta.validation.Valid;
-
 @RestController
-@RequestMapping("api/v1/messages")
 public class MessageController {
     
     private MessageService service;
@@ -31,37 +21,7 @@ public class MessageController {
     @Autowired
     public MessageController(MessageService service) {
         this.service = service;
-    }
-    
-    @PostMapping
-    public ResponseEntity<ResponseDto> create(@Valid @RequestBody MessageDto message) {        
-        
-        message = service.save(message);
-
-        Map<String, String> infos = new HashMap<>();
-        infos.put("info", String.format("Message with id %d was created", message.getId()));
-
-        List<MessageDto> messages = new ArrayList<>();
-        messages.add(message);
-
-        return new ResponseEntity<>(new ResponseDto(HttpStatus.CREATED.value(), infos, messages), HttpStatus.CREATED);
-
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<ResponseDto> read(@PathVariable("id") Long id) {
-
-        MessageDto message = service.findById(id);
-            
-        Map<String, String> infos = new HashMap<>();
-        infos.put("info", String.format("Message with id %d was received", id));
-
-        List<MessageDto> messages = new ArrayList<>();
-        messages.add(message);
-
-        return new ResponseEntity<>(new ResponseDto(HttpStatus.OK.value(), infos, messages), HttpStatus.OK);
-
-    }
+    }    
 
     @GetMapping()
     public ResponseEntity<ResponseDto> readAll() {
@@ -74,34 +34,6 @@ public class MessageController {
 
         return new ResponseEntity<>(new ResponseDto(HttpStatus.OK.value(), infos, messages), HttpStatus.OK);
 
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<ResponseDto> update(@PathVariable("id") Long id, @Valid @RequestBody MessageDto messageNew) { 
-
-        MessageDto message = service.findById(id); 
-        message.setText(messageNew.getText());      
-        message = service.update(message);
-
-        Map<String, String> infos = new HashMap<>();
-        infos.put("info", String.format("Message with id %d was updated", id));
-
-        List<MessageDto> messages = new ArrayList<>();
-        messages.add(message);
-
-        return new ResponseEntity<>(new ResponseDto(HttpStatus.OK.value(), infos, messages), HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ResponseDto> delete(@PathVariable("id") Long id) {
-            
-        service.delete(id);
-
-        Map<String, String> infos = new HashMap<>();
-        infos.put("info", String.format("Message with id %d was deleted", id));
-
-        return new ResponseEntity<>(new ResponseDto(HttpStatus.OK.value(), infos, null), HttpStatus.OK);
-            
     }
 
 }
--- others/db/sql/java-springboot3-transactions-isolation-read-commited/src/main/java/com/example/service/MessageService.java
+++ others/db/sql/java-springboot3-transactions-isolation-read-commited/src/main/java/com/example/service/MessageService.java
@@ -1,8 +1,7 @@
 package com.example.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,14 +10,23 @@ import com.example.model.dto.MessageDto;
 import com.example.model.entity.MessageEntity;
 import com.example.repository.MessageRepository;
 
+import jakarta.annotation.PostConstruct;
+
 @Service
 public class MessageService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageService.class);
     
     private MessageRepository repository;
 
     @Autowired
     public MessageService(MessageRepository repository) {
         this.repository = repository;
+    }
+
+    @PostConstruct
+    public void setUp() {
+        repository.save(new MessageEntity("Hello World"));
     }
     
     public MessageDto save(MessageDto message) {
@@ -34,30 +42,15 @@ public class MessageService {
         return new MessageDto(entity.getId(), entity.getText());
     }
 
-    public List<MessageDto> findAll() {
-
-        List<MessageDto> dtos = new ArrayList<>();
-
-        List<MessageEntity> entities = repository.findAll();
-        for (MessageEntity entity : entities) {
-            dtos.add(new MessageDto(entity.getId(), entity.getText()));
-        }
-        
-        return dtos;
-
-    }
-
-    public MessageDto update(MessageDto message) {
+    public void updateWithError(MessageDto message) {
 
         MessageEntity entity = new MessageEntity(message.getId(), message.getText());
         entity = repository.save(entity);
-        return new MessageDto(entity.getId(), entity.getText());
-        
-    }
 
-    public void delete(Long id) throws MessageException {        
-        MessageEntity entity = repository.findById(id).orElseThrow(() -> new MessageException("There is no Message with id: " + id));
-        repository.delete(entity);
+        logger.info("Object Message with id {} was created", entity.getId());
+
+        if (true)
+            throw new MessageException("Example Exception");
         
     }
 
