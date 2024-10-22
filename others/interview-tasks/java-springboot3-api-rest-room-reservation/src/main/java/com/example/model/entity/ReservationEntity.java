50f887449bc9af95601c265334cc516ca7480ae8
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-10-22 16:20:25 +0200

    Improvement ReservationEntity

--- others/interview-tasks/java-springboot3-api-rest-room-reservation/src/main/java/com/example/model/entity/ReservationEntity.java
+++ others/interview-tasks/java-springboot3-api-rest-room-reservation/src/main/java/com/example/model/entity/ReservationEntity.java
@@ -1,5 +1,7 @@
 package com.example.model.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,23 +9,24 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "MESSAGE")
+@Table(name = "RESERVATION")
 public class ReservationEntity {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String text;    
+    private String roomName;    
+    private String reservedBy;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 
     public ReservationEntity() {}
 
-    public ReservationEntity(Long id, String text) {
-        this.id = id;
-        this.text = text;
-    }    
-
-    public ReservationEntity(String text) {
-        this.text = text;
+    public ReservationEntity(String roomName, String reservedBy, LocalDateTime startTime, LocalDateTime endTime) {
+        this.roomName = roomName;
+        this.reservedBy = reservedBy;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
     public Long getId() {
@@ -32,11 +35,29 @@ public class ReservationEntity {
     public void setId(Long id) {
         this.id = id;
     }
-    public String getText() {
-        return text;
+    public String getRoomName() {
+        return roomName;
     }
-    public void setText(String text) {
-        this.text = text;
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+    public String getReservedBy() {
+        return reservedBy;
+    }
+    public void setReservedBy(String reservedBy) {
+        this.reservedBy = reservedBy;
+    }
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
     }    
 
 }
