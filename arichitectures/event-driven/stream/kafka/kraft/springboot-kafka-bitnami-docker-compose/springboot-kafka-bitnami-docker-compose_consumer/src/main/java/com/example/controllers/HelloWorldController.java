89502280faa24b51755e94d85b5acdbcf2f12937
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-01-23 17:32:13 +0100

    Update controller of Consumer

--- arichitectures/event-driven/stream/kafka/kraft/springboot-kafka-bitnami-docker-compose/springboot-kafka-bitnami-docker-compose_consumer/src/main/java/com/example/controllers/HelloWorldController.java
+++ arichitectures/event-driven/stream/kafka/kraft/springboot-kafka-bitnami-docker-compose/springboot-kafka-bitnami-docker-compose_consumer/src/main/java/com/example/controllers/HelloWorldController.java
@@ -2,13 +2,22 @@ package com.example.controllers;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HelloWorldController {
+
+	private String message = "There is no message from Producer via Kafka yet";
+
+	@GetMapping
+	public String helloWorld() {		
+		return "Message from Producer via Kafka is: " + message;
+		
+	}
 		
 	@KafkaListener(topics = "#{'${topic.name}'}")
-	public void helloWorldListener(String message) {		
+	public void helloWorldListener(String message) {
+		this.message = message;		
 		System.out.println(message);		
 	}
 	
