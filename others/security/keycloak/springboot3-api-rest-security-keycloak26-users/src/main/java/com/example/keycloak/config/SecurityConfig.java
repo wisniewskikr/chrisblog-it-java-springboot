c087c52b52f3f1a1f90a249e245f22d5fa5de5fa
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-02-21 12:22:57 +0100

    Update spring boot version

--- others/security/keycloak/springboot3-api-rest-security-keycloak26-users/src/main/java/com/example/keycloak/config/SecurityConfig.java
+++ others/security/keycloak/springboot3-api-rest-security-keycloak26-users/src/main/java/com/example/keycloak/config/SecurityConfig.java
@@ -2,10 +2,11 @@ package com.example.keycloak.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -13,23 +14,23 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        
         http
-                .csrf()
-                    .disable()
-                .authorizeHttpRequests()
-                    .requestMatchers("/api/v1/demo")
-                        .permitAll()
-                    .anyRequest()
-                        .authenticated();
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/demo").permitAll()
+                .anyRequest().authenticated()
+            );
 
         http
-                .oauth2ResourceServer()
-                    .jwt();
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(Customizer.withDefaults())
+            );
 
         http
-                .sessionManagement()
-                    .sessionCreationPolicy(STATELESS);
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+
     }
 }
