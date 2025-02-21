0c8503dc2930129294ccba8b53d464b6c0d2c358
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-02-21 12:40:49 +0100

    Update Spring Boot version

--- others/security/keycloak/springboot3-api-rest-security6-keycloak-users-roles/src/main/java/com/example/keycloak/config/SecurityConfig.java
+++ others/security/keycloak/springboot3-api-rest-security6-keycloak-users-roles/src/main/java/com/example/keycloak/config/SecurityConfig.java
@@ -4,17 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.keycloak.converter.JwtAuthConverter;
 
 import lombok.RequiredArgsConstructor;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
 @Configuration
-@EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -23,24 +20,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        
         http
-                .csrf()
-                    .disable()
-                .authorizeHttpRequests()
-                    .requestMatchers("/api/v1/demo")
-                        .permitAll()
-                    .anyRequest()
-                        .authenticated();
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/demo").permitAll()
+                .anyRequest().authenticated()
+            );
+
+            http
+                .oauth2ResourceServer(oauth2 -> 
+                    oauth2.jwt(jwt -> 
+                        jwt.jwtAuthenticationConverter(jwtAuthConverter)
+                    )
+                );
 
         http
-                .oauth2ResourceServer()
-                    .jwt()
-                        .jwtAuthenticationConverter(jwtAuthConverter);
-
-        http
-                .sessionManagement()
-                    .sessionCreationPolicy(STATELESS);
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+
     }
 }
