27c9a2c0bbf81f28fe1140e8a53d482bc25fe8bf
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-02-22 16:37:51 +0100

    Add roles to application

--- others/security/keycloak/springboot3-api-rest-security-resourceserver-authorizationcode-keycloak-roles/src/main/java/com/example/keycloak/config/SecurityConfig.java
+++ others/security/keycloak/springboot3-api-rest-security-resourceserver-authorizationcode-keycloak-roles/src/main/java/com/example/keycloak/config/SecurityConfig.java
@@ -2,15 +2,21 @@ package com.example.keycloak.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.keycloak.converter.JwtAuthConverter;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,8 +29,10 @@ public class SecurityConfig {
             );
 
         http
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(Customizer.withDefaults())
+            .oauth2ResourceServer(oauth2 -> 
+                oauth2.jwt(jwt -> 
+                    jwt.jwtAuthenticationConverter(jwtAuthConverter)
+                )
             );
 
         http
--- others/security/keycloak/springboot3-api-rest-security-resourceserver-authorizationcode-keycloak-roles/src/main/java/com/example/keycloak/controller/DemoController.java
+++ others/security/keycloak/springboot3-api-rest-security-resourceserver-authorizationcode-keycloak-roles/src/main/java/com/example/keycloak/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.keycloak.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +15,13 @@ public class DemoController {
     }
 
     @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public String helloUser() {
         return "Hello World, User!";
     }
 
     @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
     public String helloAdmin() {
         return "Hello World, Admin!";
     }
