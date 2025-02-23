830762a799c59d809739d1d269470f7bb6f55c32
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-02-23 11:53:33 +0100

    Init project

---
+++ others/security/keycloak/springboot3-api-rest-security-resourceserver-implicit-keycloak-users/src/main/java/com/example/keycloak/controller/DemoController.java
@@ -0,0 +1,26 @@
+package com.example.keycloak.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/user")
+    public String helloUser() {
+        return "Hello World, User!";
+    }
+
+    @GetMapping("/admin")
+    public String helloAdmin() {
+        return "Hello World, Admin!";
+    }
+
+}
