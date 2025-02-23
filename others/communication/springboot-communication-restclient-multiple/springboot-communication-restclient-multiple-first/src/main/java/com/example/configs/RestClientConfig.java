dcdfd37842d06b362ca107262549e59b2fd27a82
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-02-23 17:43:38 +0100

    Add RestClient

---
+++ others/communication/springboot-communication-restclient-multiple/springboot-communication-restclient-multiple-first/src/main/java/com/example/configs/RestClientConfig.java
@@ -0,0 +1,17 @@
+package com.example.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+	
+	@Bean
+    RestClient restClient() {
+		return RestClient.builder()
+			.baseUrl("https://jsonplaceholder.typicode.com")
+			.build();
+	}
+
+}
