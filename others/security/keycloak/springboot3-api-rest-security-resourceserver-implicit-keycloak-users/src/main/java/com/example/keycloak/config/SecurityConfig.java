14c24372fbef6a09042b75a9b8d55722a22d73b9
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-02-23 12:27:40 +0100

    Remove CodeController

--- others/security/keycloak/springboot3-api-rest-security-resourceserver-implicit-keycloak-users/src/main/java/com/example/keycloak/config/SecurityConfig.java
+++ others/security/keycloak/springboot3-api-rest-security-resourceserver-implicit-keycloak-users/src/main/java/com/example/keycloak/config/SecurityConfig.java
@@ -4,12 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -18,7 +16,7 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/v1/demo", "/code").permitAll()
+                .requestMatchers("/api/v1/demo").permitAll()
                 .anyRequest().authenticated()
             );
 
