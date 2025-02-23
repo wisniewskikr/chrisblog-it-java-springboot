b1333fe04fb22b40e8adf7388ca606ea945ab743
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-02-23 17:59:20 +0100

    Update first service

--- others/communication/springboot-communication-restclient-multiple/springboot-communication-restclient-multiple-first/src/main/java/com/example/controller/HelloWorldController.java
+++ others/communication/springboot-communication-restclient-multiple/springboot-communication-restclient-multiple-first/src/main/java/com/example/controller/HelloWorldController.java
@@ -1,10 +1,23 @@
 package com.example.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.dto.HelloWorldDto;
+import com.example.service.HelloWorldService;
+
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class HelloWorldController {
 	
-	
+	private final HelloWorldService helloWorldService;
+
+    @GetMapping(value="/")
+	public ResponseEntity<HelloWorldDto> helloWorld() {    
+        return ResponseEntity.ok(helloWorldService.getMessage());    
+    }
 	
 }
