11d36d82383c2ec25163f66626158d185082cc46
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-02-24 06:23:53 +0100

    Update HelloWorldApplication for second app

--- others/communication/springboot-communication-restclient-multiple/springboot-communication-restclient-multiple-second/src/main/java/com/example/HelloWorldApplication.java
+++ others/communication/springboot-communication-restclient-multiple/springboot-communication-restclient-multiple-second/src/main/java/com/example/HelloWorldApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorldBeApplication {
+public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldBeApplication.class, args);
+		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
 }
