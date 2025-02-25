79f647371aa2c3cbea28bc38caeb0453b805c835
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-02-25 18:00:51 +0100

    Fix log out

--- others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-authorizationcode-keycloak-users/src/main/java/com/example/configs/SecurityConfig.java
+++ others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-authorizationcode-keycloak-users/src/main/java/com/example/configs/SecurityConfig.java
@@ -5,14 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
 
         http
             .authorizeHttpRequests(auth -> auth
@@ -24,12 +27,29 @@ public class SecurityConfig {
             .oauth2Login(Customizer.withDefaults());
 
         http
+            .logout(logout -> logout
+                .logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository))
+            );
+
+        http
             .exceptionHandling(exception -> exception
                 .accessDeniedPage("/access-denied")
             );
         
         return http.build();
         
+    }
+
+    @Bean
+    public LogoutSuccessHandler oidcLogoutSuccessHandler(ClientRegistrationRepository clientRegistrationRepository) {
+
+        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler = 
+            new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        
+        // Configure the post-logout redirect URI
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:9090/");
+        
+        return oidcLogoutSuccessHandler;
     }
 
 }
