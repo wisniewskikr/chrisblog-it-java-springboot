c7bd24e88023041e922406094466e947836176f2
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-02-25 17:40:00 +0100

    Update SecurityConfig

--- others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-authorizationcode-keycloak-users/src/main/java/com/example/configs/SecurityConfig.java
+++ others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-authorizationcode-keycloak-users/src/main/java/com/example/configs/SecurityConfig.java
@@ -2,41 +2,31 @@ package com.example.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http            
-            .authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/user").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
-                .requestMatchers("/admin").hasAnyAuthority("ROLE_ADMIN")
-                .anyRequest().permitAll()
-			)
-            .formLogin(login -> 
-                login
-                    .loginPage("/login"))
-            .logout(logout -> 
-                logout
-                    .logoutUrl("/logout")
-                    .logoutSuccessUrl("/")
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID")
-                    .permitAll()
-            )
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login", "/public").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        http
+            .oauth2Login(Customizer.withDefaults());
+
+        http
             .exceptionHandling(exception -> exception
                 .accessDeniedPage("/access-denied")
-            )
-            .csrf(csrf -> csrf
-                .ignoringRequestMatchers("/h2-console/**"))
-            .headers(httpSecurityHeadersConfigurer -> {
-                httpSecurityHeadersConfigurer.frameOptions(FrameOptionsConfig::disable);
-            });
+            );
         
         return http.build();
         
