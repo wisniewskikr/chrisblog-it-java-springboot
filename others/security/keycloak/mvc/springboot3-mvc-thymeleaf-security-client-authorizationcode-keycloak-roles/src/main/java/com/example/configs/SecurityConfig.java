55eba661cb94e3778068356e41e5c6d270ee3eae
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-03-01 06:51:05 +0100

    Implement roles

--- others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-authorizationcode-keycloak-roles/src/main/java/com/example/configs/SecurityConfig.java
+++ others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-authorizationcode-keycloak-roles/src/main/java/com/example/configs/SecurityConfig.java
@@ -1,13 +1,24 @@
 package com.example.configs;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import com.example.users.CustomOidcUser;
 
 @Configuration
 public class SecurityConfig {
@@ -22,7 +33,9 @@ public class SecurityConfig {
             );
 
         http
-            .oauth2Login(Customizer.withDefaults());
+            .oauth2Login(oauth2 -> oauth2
+                .userInfoEndpoint(userInfo -> userInfo.oidcUserService(this.oidcUserService()))
+            );
 
         http
             .logout(logout -> logout
@@ -48,6 +61,30 @@ public class SecurityConfig {
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:9090/");
         
         return oidcLogoutSuccessHandler;
+    }
+
+     @Bean
+    public OidcUserService oidcUserService() {
+        return new OidcUserService() {
+            @Override
+            public OidcUser loadUser(OidcUserRequest userRequest) {
+                OidcUser oidcUser = super.loadUser(userRequest);
+
+                Set<GrantedAuthority> mappedAuthorities = new HashSet<>(oidcUser.getAuthorities());
+
+                // Extract roles from the token
+                Collection<String> roles = oidcUser.getClaimAsStringList("roles");
+                if (roles != null) {
+                    mappedAuthorities.addAll(
+                        roles.stream()
+                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()))
+                            .collect(Collectors.toSet())
+                    );
+                }
+
+                return new CustomOidcUser(mappedAuthorities, oidcUser);
+            }
+        };
     }
 
 }
--- others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-authorizationcode-keycloak-roles/src/main/java/com/example/controllers/UserController.java
+++ others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-authorizationcode-keycloak-roles/src/main/java/com/example/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.example.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
 	@RequestMapping(value="/user")
+	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
 	public String display() {
 		return "user";		
 	}
