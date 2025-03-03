8d09a933c70776eb9ee1dcb2c6f1b2846a702a67
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-03-03 07:13:02 +0100

    Fix SecurityConfig

--- others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-authorizationcode-keycloak-roles/src/main/java/com/example/configs/SecurityConfig.java
+++ others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-authorizationcode-keycloak-roles/src/main/java/com/example/configs/SecurityConfig.java
@@ -1,27 +1,38 @@
 package com.example.configs;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import com.example.users.CustomOidcUser;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
+import org.springframework.core.convert.converter.Converter;
 
 @Configuration
+@RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Value("${jwt.auth.converter.resource-id}")
+    private String resourceId;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
@@ -33,14 +44,15 @@ public class SecurityConfig {
             );
 
         http
-            .oauth2Login(oauth2 -> oauth2
-                .userInfoEndpoint(userInfo -> userInfo.oidcUserService(this.oidcUserService()))
-            );
+            .oauth2Login(Customizer.withDefaults());
 
         http
             .logout(logout -> logout
                 .logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository))
             );
+
+        http
+            .oauth2Login(Customizer.withDefaults());
 
         http
             .exceptionHandling(exception -> exception
@@ -49,6 +61,46 @@ public class SecurityConfig {
         
         return http.build();
         
+    }
+
+    interface AuthoritiesConverter extends Converter<Map<String, Object>, Collection<GrantedAuthority>> {}
+
+    @Bean
+    AuthoritiesConverter realmRolesAuthoritiesConverter() {
+        return claims -> {            
+            Map<String, Object> resourceAccess;
+            Map<String, Object> resource;
+            Collection<String> resourceRoles;
+            if (claims.get("resource_access") == null) {
+                return Set.of();
+            }
+            resourceAccess = (Map<String, Object>) claims.get("resource_access");
+
+            if (resourceAccess.get(resourceId) == null) {
+                return Set.of();
+            }
+            resource = (Map<String, Object>) resourceAccess.get(resourceId);
+
+            resourceRoles = (Collection<String>) resource.get("roles");
+            return resourceRoles
+                    .stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toSet());
+        };
+    }
+
+    @Bean
+    GrantedAuthoritiesMapper authenticationConverter(
+    Converter<Map<String, Object>, Collection<GrantedAuthority>> realmRolesAuthoritiesConverter) {
+
+        return (authorities) -> authorities.stream()
+                .filter(authority -> authority instanceof OidcUserAuthority)
+                .map(OidcUserAuthority.class::cast)
+                .map(OidcUserAuthority::getIdToken)
+                .map(OidcIdToken::getClaims)
+                .map(realmRolesAuthoritiesConverter::convert)
+                .flatMap(roles -> roles.stream())
+                .collect(Collectors.toSet());
     }
 
     @Bean
@@ -61,30 +113,6 @@ public class SecurityConfig {
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:9090/");
         
         return oidcLogoutSuccessHandler;
-    }
-
-     @Bean
-    public OidcUserService oidcUserService() {
-        return new OidcUserService() {
-            @Override
-            public OidcUser loadUser(OidcUserRequest userRequest) {
-                OidcUser oidcUser = super.loadUser(userRequest);
-
-                Set<GrantedAuthority> mappedAuthorities = new HashSet<>(oidcUser.getAuthorities());
-
-                // Extract roles from the token
-                Collection<String> roles = oidcUser.getClaimAsStringList("roles");
-                if (roles != null) {
-                    mappedAuthorities.addAll(
-                        roles.stream()
-                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()))
-                            .collect(Collectors.toSet())
-                    );
-                }
-
-                return new CustomOidcUser(mappedAuthorities, oidcUser);
-            }
-        };
-    }
+    }   
 
 }
