5f40b4a85e6f4a134f77f3d3e22ed2a8cd48a988
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-03-11 11:56:54 +0100

    Init project

---
+++ others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-keycloak-postgres/src/main/java/com/example/controllers/LogoutController.java
@@ -0,0 +1,20 @@
+package com.example.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogoutController {
+
+	@GetMapping("/tmp")
+	public String logout(HttpServletRequest request) throws ServletException {
+		
+		request.logout();
+		return "logout";
+		
+	}
+	
+}
