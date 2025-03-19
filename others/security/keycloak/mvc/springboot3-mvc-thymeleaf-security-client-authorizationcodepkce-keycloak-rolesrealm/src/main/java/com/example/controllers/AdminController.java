658ce7c618e9bf68a25420715ca7ec35d1db33b3
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-03-19 16:42:52 +0100

    Init project

---
+++ others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-authorizationcodepkce-keycloak-rolesrealm/src/main/java/com/example/controllers/AdminController.java
@@ -0,0 +1,14 @@
+package com.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+
+	@RequestMapping(value="/admin")
+	public String display() {
+		return "admin";		
+	}
+	
+}
