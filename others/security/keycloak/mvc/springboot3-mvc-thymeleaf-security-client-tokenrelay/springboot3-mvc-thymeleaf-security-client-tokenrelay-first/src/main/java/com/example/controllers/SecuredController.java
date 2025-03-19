807c94a092dc08d124f8ae856856102dee4c3e4b
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-03-19 17:50:24 +0100

    Update first application

--- others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-tokenrelay/springboot3-mvc-thymeleaf-security-client-tokenrelay-first/src/main/java/com/example/controllers/SecuredController.java
+++ others/security/keycloak/mvc/springboot3-mvc-thymeleaf-security-client-tokenrelay/springboot3-mvc-thymeleaf-security-client-tokenrelay-first/src/main/java/com/example/controllers/SecuredController.java
@@ -1,13 +1,22 @@
 package com.example.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.service.HelloWorldService;
+
+import lombok.AllArgsConstructor;
+
 @Controller
+@AllArgsConstructor
 public class SecuredController {
 
+	private final HelloWorldService helloWorldService;
+
 	@RequestMapping(value="/secured")
-	public String display() {
+	public String display(Model model) {
+		model.addAttribute("message", helloWorldService.getMessage());
 		return "secured";		
 	}
 	
