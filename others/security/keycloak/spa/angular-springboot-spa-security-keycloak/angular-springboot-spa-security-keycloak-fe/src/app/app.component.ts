2297c0c26a84267327ba7afa43ddbc655fc495c7
Chris <wisniewskikr@gmail.com>
2025-03-26 16:56:13 +0100

    Update app component

--- others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/app.component.ts
+++ others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiService } from './services/api.service';
+import { OAuthService } from 'angular-oauth2-oidc';
+import {authConfig} from "./auth.config";
 
 @Component({
   selector: 'app-root',
@@ -13,13 +15,28 @@ export class AppComponent implements OnInit{
 
   message: any;
 
-  constructor(private service: ApiService) {}
+  constructor(private service: ApiService, private oauthService: OAuthService) {
+    this.configure();
+  }
 
   ngOnInit(): void {
     let response = this.service.getMessage();
     response.subscribe((data)=>{      
       this.message = data
     });
+  }
+
+  private configure() {
+    this.oauthService.configure(authConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  login() {
+    this.oauthService.initCodeFlow();
+  }
+
+  logout() {
+    this.oauthService.logOut();
   }
   
 }
