f1d86871df89eeab681d00f3ed10d772b3f65e06
Chris <wisniewskikr@gmail.com>
2025-03-27 16:44:40 +0100

    Update oidc version

--- others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/app.config.ts
+++ others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { OAuthModule } from 'angular-oauth2-oidc';
+import { OAuthModule,provideOAuthClient } from 'angular-oauth2-oidc';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideHttpClient(withFetch()),
+    provideOAuthClient(),
     importProvidersFrom(OAuthModule.forRoot({
       resourceServer: {
         allowedUrls: ['http://localhost:9090'],
