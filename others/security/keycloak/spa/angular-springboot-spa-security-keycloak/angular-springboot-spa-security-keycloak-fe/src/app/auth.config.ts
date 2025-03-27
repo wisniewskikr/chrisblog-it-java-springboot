e329b431d7baabbb57ba164ebebd8d742828e8d0
Chris <wisniewskikr@gmail.com>
2025-03-27 12:35:05 +0100

    Add realm json file

--- others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/auth.config.ts
+++ others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/auth.config.ts
@@ -1,9 +1,9 @@
 import {AuthConfig} from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
-  issuer: 'http://localhost:8180/auth/realms/oauth2-demo-realm',
+  issuer: 'http://localhost:8080/realms/helloworld-realm',
   redirectUri: window.location.origin,
-  clientId: 'oauth2-demo-pkce-client',
+  clientId: 'helloworld-client',
   responseType: 'code',
   strictDiscoveryDocumentValidation: false,
   scope: 'openid profile email offline_access',
