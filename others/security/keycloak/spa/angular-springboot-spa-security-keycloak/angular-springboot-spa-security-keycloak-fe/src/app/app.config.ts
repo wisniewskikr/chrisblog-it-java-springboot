361f4021ba59ccb5b46952e31f689bd6cc0b4c93
Chris <wisniewskikr@gmail.com>
2025-03-27 11:53:18 +0100

    Add OAuthModule

--- others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/app.config.ts
+++ others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/app.config.ts
@@ -1,9 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch())]
+  providers: [
+    provideRouter(routes), 
+    provideHttpClient(withFetch()),
+    importProvidersFrom(OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ['http://localhost:8080/api'],
+        sendAccessToken: true
+      }
+    }))
+  ]
 };
