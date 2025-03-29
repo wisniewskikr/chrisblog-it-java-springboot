aec135dba3b599a8f8acabebae931ead43ef449c
Chris <wisniewskikr@gmail.com>
2025-03-29 08:37:13 +0100

    Add logout to public page

--- others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/components/public/public.component.ts
+++ others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/components/public/public.component.ts
@@ -1,25 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-public',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './public.component.html',
   styleUrl: './public.component.css'
 })
 export class PublicComponent implements OnInit {
 
+  showLogout: boolean = false;
   message: any;
 
-  constructor(private service: ApiService) {}
+  constructor(private readonly service: ApiService, private readonly authService: AuthService) {}
 
   ngOnInit(): void {
+
+    if (this.authService.isLoggedIn) {
+      this.showLogout = true;
+    }
+
     let response = this.service.getMessagePublic();
     response.subscribe((data)=>{      
       this.message = data
     });
+
+  }
+
+  logout() {
+    this.authService.logout();
   }
 
 }
