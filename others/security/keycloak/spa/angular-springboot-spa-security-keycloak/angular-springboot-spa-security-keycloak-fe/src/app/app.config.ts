c775e9b31f120d292b728034749d295997a9151c
Chris <wisniewskikr@gmail.com>
2025-03-29 07:39:08 +0100

    Comment unused code

--- others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/app.config.ts
+++ others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/app.config.ts
@@ -1,20 +1,20 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { OAuthModule,provideOAuthClient } from 'angular-oauth2-oidc';
+import { provideOAuthClient } from 'angular-oauth2-oidc';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideHttpClient(withFetch()),
-    provideOAuthClient(),
-    importProvidersFrom(OAuthModule.forRoot({
-      resourceServer: {
-        allowedUrls: ['http://localhost:9090'],
-        sendAccessToken: true
-      }
-    }))
+    provideOAuthClient()
+    // importProvidersFrom(OAuthModule.forRoot({
+    //   resourceServer: {
+    //     allowedUrls: ['http://localhost:9090'],
+    //     sendAccessToken: true
+    //   }
+    // }))
   ]
 };
