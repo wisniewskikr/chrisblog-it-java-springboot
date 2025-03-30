a75d61635ee5681a006202fa51f88d4f54a9e5c3
Chris <wisniewskikr@gmail.com>
2025-03-30 08:48:54 +0200

    Fix docker compose

--- others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/auth.config.ts
+++ others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/auth.config.ts
@@ -1,10 +1,11 @@
 import {AuthConfig} from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
-  issuer: 'http://localhost:8080/realms/helloworld-realm',
+  issuer: 'http://keycloak:8080/realms/helloworld-realm',
   redirectUri: 'http://localhost:4200/',
   clientId: 'helloworld-client',
   responseType: 'code',
   strictDiscoveryDocumentValidation: false,
   scope: 'openid profile email offline_access',
+  requireHttps: false
 }
