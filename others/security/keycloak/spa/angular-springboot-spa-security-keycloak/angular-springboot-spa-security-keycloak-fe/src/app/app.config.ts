2f1673cb2bfe4d7e73e7eb0315e8d3e491f81366
Chris <wisniewskikr@gmail.com>
2025-03-30 08:20:28 +0200

    Add authentication header authometically

--- others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/app.config.ts
+++ others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/app.config.ts
@@ -1,20 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideOAuthClient } from 'angular-oauth2-oidc';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { OAuthModule, provideOAuthClient } from 'angular-oauth2-oidc';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideHttpClient(withFetch()),
-    provideOAuthClient()
-    // importProvidersFrom(OAuthModule.forRoot({
-    //   resourceServer: {
-    //     allowedUrls: ['http://localhost:9090'],
-    //     sendAccessToken: true
-    //   }
-    // }))
+    provideOAuthClient(),
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ['http://localhost:9090/secured'],
+        sendAccessToken: true
+      }
+    }))
   ]
 };
--- others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/services/api.service.ts
+++ others/security/keycloak/spa/angular-springboot-spa-security-keycloak/angular-springboot-spa-security-keycloak-fe/src/app/services/api.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { OAuthService } from 'angular-oauth2-oidc';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +11,7 @@ export class ApiService {
   URL_PUBLIC = environment.API_BE_PUBLIC_URL;
   URL_SECURED = environment.API_BE_SECURED_URL;
 
-  constructor(private http: HttpClient, private oauthService: OAuthService) {}
+  constructor(private readonly http: HttpClient) {}
 
   getMessagePublic() {
 
@@ -27,13 +26,7 @@ export class ApiService {
 
   getMessageSecured() {
 
-    // TODO: make it authomatic
-    const headers = new HttpHeaders({
-      'Content-Type': 'text/plain; charset=utf-8',
-      'Authorization': 'Bearer ' + this.oauthService.getAccessToken()
-    });
-
-    return this.http.get(this.URL_SECURED, { headers, responseType: 'text' })
+    return this.http.get(this.URL_SECURED, { responseType: 'text' })
           .pipe(
             catchError((error) => {
               return throwError(() => error);
