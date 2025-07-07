0b734b22c047335304a2268790e6203c7032c1ab
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-07-07 12:24:26 +0200

    Init project

---
+++ arichitectures/ui/server-send-events/java-springboot3-sse-eventfe/src/test/java/com/example/javaspringboot3sseeventfe/JavaSpringboot3SseEventfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringboot3sseeventfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringboot3SseEventfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
