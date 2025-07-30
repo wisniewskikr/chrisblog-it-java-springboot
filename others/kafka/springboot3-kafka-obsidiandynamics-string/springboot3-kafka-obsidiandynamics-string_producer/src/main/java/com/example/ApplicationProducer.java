0cdf17602befeab1f9cca5f1de32bd923a449b07
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-07-30 06:52:00 +0200

    Update Application names

--- others/kafka/springboot3-kafka-obsidiandynamics-string/springboot3-kafka-obsidiandynamics-string_producer/src/main/java/com/example/ApplicationProducer.java
+++ others/kafka/springboot3-kafka-obsidiandynamics-string/springboot3-kafka-obsidiandynamics-string_producer/src/main/java/com/example/ApplicationProducer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ApplicationProducer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ApplicationProducer.class, args);
 	}
 
 }
