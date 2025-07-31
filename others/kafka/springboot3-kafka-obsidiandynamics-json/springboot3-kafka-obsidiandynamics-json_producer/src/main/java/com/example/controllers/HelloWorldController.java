ef19ba59c3a5a0131d145964c00231d0afc3fee9
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-07-31 07:13:22 +0200

    Update Producer

--- others/kafka/springboot3-kafka-obsidiandynamics-json/springboot3-kafka-obsidiandynamics-json_producer/src/main/java/com/example/controllers/HelloWorldController.java
+++ others/kafka/springboot3-kafka-obsidiandynamics-json/springboot3-kafka-obsidiandynamics-json_producer/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.controllers;
 
+import com.example.models.HelloWorldModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -9,13 +10,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/producer")
 public class HelloWorldController {	
 	
-	private final KafkaTemplate<String, String> kafkaTemplate;
+	private final KafkaTemplate<String, HelloWorldModel> kafkaTemplate;
 	
 	@Value("${topic.name}")
 	private String topicName;
 
 	@Autowired
-	public HelloWorldController(KafkaTemplate<String, String> kafkaTemplate) {
+	public HelloWorldController(KafkaTemplate<String, HelloWorldModel> kafkaTemplate) {
 		this.kafkaTemplate = kafkaTemplate;
 	}
 
@@ -23,7 +24,7 @@ public class HelloWorldController {
 	public String helloWorld(@RequestParam String name) {
 		
 		String message = "Hello World " + name;
-		kafkaTemplate.send(topicName, message);
+		kafkaTemplate.send(topicName, new HelloWorldModel(message));
 		return "The message was sent to Consumer via Kafka";
 		
 	}
