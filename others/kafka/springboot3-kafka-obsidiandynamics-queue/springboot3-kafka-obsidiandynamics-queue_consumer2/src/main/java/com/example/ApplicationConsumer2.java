1f0c9ef74451b32e133301ee75597573b05efe0c
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-08-03 09:40:32 +0200

    Add Consumer2

---
+++ others/kafka/springboot3-kafka-obsidiandynamics-queue/springboot3-kafka-obsidiandynamics-queue_consumer2/src/main/java/com/example/ApplicationConsumer2.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationConsumer2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationConsumer2.class, args);
+	}
+
+}
