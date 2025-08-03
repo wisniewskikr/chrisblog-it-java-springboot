317a8cb3a39c018b13f878f0ea2f0467a26b8e64
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-08-03 19:38:53 +0200

    Init project

---
+++ others/kafka/springboot3-kafka-obsidiandynamics-topic/springboot3-kafka-obsidiandynamics-topic_consumer1/src/main/java/com/example/ApplicationConsumer1.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationConsumer1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationConsumer1.class, args);
+	}
+
+}
