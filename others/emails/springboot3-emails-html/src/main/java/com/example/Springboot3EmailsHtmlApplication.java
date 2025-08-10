53b74c0abceff9fd1e6bfcc20a1902f5eff092d7
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-08-10 12:37:06 +0200

    Update Readme.md file

--- others/emails/springboot3-emails-html/src/main/java/com/example/Springboot3EmailsHtmlApplication.java
+++ others/emails/springboot3-emails-html/src/main/java/com/example/Springboot3EmailsHtmlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springboot3EmailsTextApplication {
+public class Springboot3EmailsHtmlApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot3EmailsTextApplication.class, args);
+        SpringApplication.run(Springboot3EmailsHtmlApplication.class, args);
     }
 
 }
