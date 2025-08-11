c82d059d7f34ba99ce954a3957dad42b44b697f6
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-08-11 12:37:47 +0200

    Add model classes

---
+++ others/payment/springboot3-payment-stripe-api/src/main/java/com/example/model/StripeRequest.java
@@ -0,0 +1,15 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StripeRequest {
+    private Long amount;
+    private Long quantity;
+    private String name;
+    private String currency;
+}
