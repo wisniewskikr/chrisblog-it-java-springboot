408d4ecc245844eca24b770ec38714c34b836f17
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-08-13 12:44:48 +0200

    Init project

---
+++ others/payment/springboot3-payment-stripe-session/src/main/java/com/example/Springboot3PaymentStripeSessionApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3PaymentStripeSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3PaymentStripeSessionApplication.class, args);
+    }
+
+}
