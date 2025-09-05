c9386c1030797fd0a161b404977fd6ea77e9e128
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-09-05 10:45:20 +0200

    Init project springboot3-circuitbreaker-resilience4j

---
+++ others/circuit-breaker/springboot3-circuitbreaker-resilience4j/src/main/java/com/example/Springboot3CircuitbreakerResilience4jApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3CircuitbreakerResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3CircuitbreakerResilience4jApplication.class, args);
+	}
+
+}
