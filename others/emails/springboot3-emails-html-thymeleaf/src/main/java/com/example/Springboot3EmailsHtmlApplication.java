c78e6a6927464ea0be87ef933fe3f22cc071c644
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-09-15 17:05:30 +0200

    Init project

---
+++ others/emails/springboot3-emails-html-thymeleaf/src/main/java/com/example/Springboot3EmailsHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3EmailsHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3EmailsHtmlApplication.class, args);
+    }
+
+}
