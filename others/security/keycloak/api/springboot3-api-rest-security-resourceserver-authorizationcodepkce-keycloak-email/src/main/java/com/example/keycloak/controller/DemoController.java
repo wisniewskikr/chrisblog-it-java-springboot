11c738cf953b352157c4878dfb9ac9113d580620
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-10-23 12:41:11 +0200

    Update DemoController class

--- others/security/keycloak/api/springboot3-api-rest-security-resourceserver-authorizationcodepkce-keycloak-email/src/main/java/com/example/keycloak/controller/DemoController.java
+++ others/security/keycloak/api/springboot3-api-rest-security-resourceserver-authorizationcodepkce-keycloak-email/src/main/java/com/example/keycloak/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.example.keycloak.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,15 @@ public class DemoController {
 
     @GetMapping("/public")
     public String hello() {
-        return "Hello World!";
+        return "Hello World, Public!";
     }
 
     @GetMapping("/user")
-    public String helloUser() {
-        return "Hello World, User!";
+    public String helloUser(@AuthenticationPrincipal Jwt jwt) {
+
+        String email = jwt.getClaim("email");
+        return "Hello World, " + email + "!";
+
     }
 
 }
