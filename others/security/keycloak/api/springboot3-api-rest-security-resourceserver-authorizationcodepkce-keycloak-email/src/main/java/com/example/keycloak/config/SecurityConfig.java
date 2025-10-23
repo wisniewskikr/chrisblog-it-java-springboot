315a7c8edec0b2e5b5d4acbbb5f0d267ce3226e9
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-10-23 12:31:43 +0200

    Update SecurityConfig class

--- others/security/keycloak/api/springboot3-api-rest-security-resourceserver-authorizationcodepkce-keycloak-email/src/main/java/com/example/keycloak/config/SecurityConfig.java
+++ others/security/keycloak/api/springboot3-api-rest-security-resourceserver-authorizationcodepkce-keycloak-email/src/main/java/com/example/keycloak/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,9 +17,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         
         http
-            .csrf(csrf -> csrf.disable())
+            .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/v1/demo").permitAll()
+                .requestMatchers("/api/v1/public").permitAll()
                 .anyRequest().authenticated()
             );
 
--- others/security/keycloak/api/springboot3-api-rest-security-resourceserver-authorizationcodepkce-keycloak-email/src/main/java/com/example/keycloak/controller/DemoController.java
+++ others/security/keycloak/api/springboot3-api-rest-security-resourceserver-authorizationcodepkce-keycloak-email/src/main/java/com/example/keycloak/controller/DemoController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/demo")
+@RequestMapping("/api/v1")
 public class DemoController {
 
-    @GetMapping
+    @GetMapping("/public")
     public String hello() {
         return "Hello World!";
     }
@@ -16,11 +16,6 @@ public class DemoController {
     @GetMapping("/user")
     public String helloUser() {
         return "Hello World, User!";
-    }
-
-    @GetMapping("/admin")
-    public String helloAdmin() {
-        return "Hello World, Admin!";
     }
 
 }
