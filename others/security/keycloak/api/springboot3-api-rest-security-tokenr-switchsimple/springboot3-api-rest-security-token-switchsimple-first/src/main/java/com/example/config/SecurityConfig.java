38afa75e8677f878a79d745a88ed194768f15444
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2025-10-28 16:55:10 +0100

    Add Converter to First service

--- others/security/keycloak/api/springboot3-api-rest-security-tokenr-switchsimple/springboot3-api-rest-security-token-switchsimple-first/src/main/java/com/example/config/SecurityConfig.java
+++ others/security/keycloak/api/springboot3-api-rest-security-tokenr-switchsimple/springboot3-api-rest-security-token-switchsimple-first/src/main/java/com/example/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.config;
 
+import com.example.converter.JwtAuthConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,7 +11,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,12 +22,15 @@ public class SecurityConfig {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/message").hasRole("USER")
                 .anyRequest().authenticated()
             );
 
         http
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(Customizer.withDefaults())
+            .oauth2ResourceServer(oauth2 ->
+                    oauth2.jwt(jwt ->
+                            jwt.jwtAuthenticationConverter(jwtAuthConverter)
+                    )
             );
 
         http
